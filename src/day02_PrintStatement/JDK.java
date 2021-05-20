14b06a72a5a0a0b6d6350a6bc85074e33d585fcc
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-20 13:01:28 -0400

    day02

---
+++ src/day02_PrintStatement/JDK.java
@@ -0,0 +1,16 @@
+package day02_PrintStatement;
+
+public class JDK {
+
+    public static void main(String[] args) {
+        System.out.println("JDK: Java Developmnet Kit");
+        System.out.println();
+        System.out.println("JRE: java Runtime Environment");
+        System.out.println();
+        System.out.println("Javac: Java Compiler");
+        System.out.println();
+        System.out.println("JVM: Java Virtual Machine");
+
+    }
+
+}
