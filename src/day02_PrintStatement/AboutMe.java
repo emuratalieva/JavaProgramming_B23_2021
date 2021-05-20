68904585e0af840f6339ad1a9ffcd4356eec0a30
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-20 11:57:49 -0400

    day02

---
+++ src/day02_PrintStatement/AboutMe.java
@@ -0,0 +1,12 @@
+package day02_PrintStatement;
+
+public class AboutMe {
+
+    public static void main(String[] args) {
+
+        System.out.println("My name is Muhtar, I live in Virginia");
+
+    }
+
+
+}
