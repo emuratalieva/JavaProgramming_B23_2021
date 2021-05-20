1b36324de06229e0a4d0d0b1accfb56f80dedf00
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-20 12:08:22 -0400

    day02

---
+++ src/day02_PrintStatement/MultiplePrintStatements.java
@@ -0,0 +1,16 @@
+package day02_PrintStatement;
+
+public class MultiplePrintStatements {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Muhtar");
+        System.out.println("Today is Thursday");
+        System.out.println("How are you?");
+
+
+
+    }
+
+
+}
