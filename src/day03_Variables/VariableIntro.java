621a633757536ae303c80b7ebe3442bd77d05690
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-21 14:29:31 -0400

    day03

---
+++ src/day03_Variables/VariableIntro.java
@@ -0,0 +1,26 @@
+package day03_Variables;
+
+public class VariableIntro {
+
+    public static void main(String[] args) {
+
+        byte myAge = 55;
+        byte myBrothersAge = 45;
+
+        System.out.print("My age is: ");
+        System.out.println(myAge);
+
+        System.out.print("My Brother' age is: ");
+        System.out.println(myBrothersAge);
+
+        // 1975
+       // byte myBirthYear = 1975;   maximum number we can assign to byte is 127
+
+        short myBirthYear = 1975;
+        System.out.print("My Birth Year: ");
+        System.out.println(myBirthYear);
+
+
+    }
+
+}
