adee9ea0dd4d489ccef75d5a74395bd9d7afe96f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-21 11:29:04 -0400

    day03

---
+++ src/day03_Variables/EscapeSequences.java
@@ -0,0 +1,35 @@
+package day03_Variables;
+
+/*
+\n: new line (Enter)
+\t: paragraph space (tab)
+\"
+ */
+
+public class EscapeSequences {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello everyone \nHow are you all today?");
+
+        System.out.println("---------------------------------------------");
+
+        System.out.println("Hello Everyone");
+        System.out.println("My name is Muhtar");
+        System.out.println("I live in Virginia");
+
+        System.out.println("-----------------------------------");
+
+        System.out.println("Hello Everyone\nMy name is Muhtar\nI live in Virginia");
+
+        System.out.println("------------------------------------");
+        System.out.println("\tToday is a great day to study Java");
+
+        System.out.println("-----------------------------------------------");
+
+        System.out.println("My favorite TV series is \"Game of Throne\"");
+
+    }
+
+}
