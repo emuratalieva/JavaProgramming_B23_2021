7eb8cd98b1a17a3c1aa12cbac0d98f069c7d9716
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-21 15:12:26 -0400

    day03

---
+++ src/day03_Variables/CalculateSquare.java
@@ -0,0 +1,23 @@
+package day03_Variables;
+
+public class CalculateSquare {
+
+    public static void main(String[] args) {
+
+        int side = 8;
+
+        int area = side * side;
+        int perimeter = 4 * side;
+
+        System.out.println(area);
+        System.out.println(perimeter);
+        
+
+    }
+
+}
+
+/*
+write a program that can calculate the area & perimeter of any given square
+
+ */
