4bbba6a4353ffa6c5ea4f7e22932e2c5023ca702
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-21 15:00:04 -0400

    day03

---
+++ src/day03_Variables/Addition.java
@@ -0,0 +1,16 @@
+package day03_Variables;
+
+public class Addition {
+
+    public static void main(String[] args) {
+
+        int num1 = 100;
+        int num2 = 200;
+
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+
+
+    }
+}
