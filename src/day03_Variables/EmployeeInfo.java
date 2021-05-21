4aba54fb6e62e1dd0364ac584f4dd2cfcdcfc301
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-21 10:51:00 -0400

    day03

---
+++ src/day03_Variables/EmployeeInfo.java
@@ -0,0 +1,16 @@
+package day03_Variables;
+
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Company Name: Apple INC");
+        System.out.println("Employee Name: John");
+        System.out.println("Employee ID: M123");
+        System.out.println("Job Title: SDET");
+        System.out.println("Salary: $120000");
+
+
+    }
+
+}
