27235fcd504246bdd0783cbe725d3cc44c62d342
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-24 12:22:14 -0400

    day04

---
+++ src/day04_Variables/GallonsToLiters.java
@@ -0,0 +1,23 @@
+package day04_Variables;
+
+public class GallonsToLiters {
+
+    public static void main(String[] args) {
+        int gallon = 10;
+
+        double litters = gallon * 3.78541;
+
+        System.out.println("gallon = " + gallon);
+        System.out.println("litters = " + litters);
+
+    }
+
+}
+
+/*
+3. create a class called GalonsToLiters, and write a program that can convert the gallons tto
+    		Ex:
+                galon = 10;
+
+           Hint: 1 gallon = 3.78541 liters
+ */
---
+++ src/day04_Variables/PrimitivesContinue.java
@@ -0,0 +1,69 @@
+package day04_Variables;
+
+public class PrimitivesContinue {
+    /*
+    int: for integers
+    double: for decimals
+    char: for single character only. ''
+    boolean: True/False
+     */
+
+    public static void main(String[] args) {
+            // #
+        char ch1 = '#';
+        char ch2 = 35;
+
+        System.out.println("ch1 = " + ch1);
+        System.out.println("ch2 = " + ch2);
+
+        // Z
+        char ch3 = 'Z';
+        char ch4 = 90;
+
+        System.out.println("ch3 = " + ch3);
+        System.out.println("ch4 = " + ch4);
+
+        System.out.println("------------------------------");
+        char ch5 = 15000;
+        System.out.println("ch5 = " + ch5);
+        
+        char ch6 = 55000;
+        System.out.println("ch6 = " + ch6);
+
+        char ch7 = 15 + 45 ;
+        System.out.println("ch7 = " + ch7);
+
+        System.out.println("------------------------------");
+        // 10 > 9
+        boolean r1 = 10 > 9;
+        System.out.println("r1 = " + r1);
+
+        boolean r2 = true;
+        boolean r3 = false;
+
+        System.out.println("r2 = " + r2);
+        System.out.println("r3 = " + r3);
+
+        boolean isFullTime = false;
+        boolean isMarried = false;
+        boolean isPandemic = true;
+
+
+        boolean r4 =    "Java"  == "Python"; // equal operator: ==
+        System.out.println("r4 = " + r4);
+
+        boolean r5 = "Muhtar" == "Good guy";
+        System.out.println("r5 = " + r5);
+
+        /*
+        if isPandemic:
+            buy toilet papers
+         */
+        
+        
+        
+        
+    }
+
+
+}
