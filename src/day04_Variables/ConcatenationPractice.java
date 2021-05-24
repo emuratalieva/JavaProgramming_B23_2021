68943f93adb1e6ada3621f8dc1ae9f804693dfbf
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-24 15:59:57 -0400

    day04

---
+++ src/day04_Variables/ConcatenationPractice.java
@@ -0,0 +1,26 @@
+package day04_Variables;
+
+public class ConcatenationPractice {
+
+    public static void main(String[] args) {
+        /*
+            +:
+                addition: when we have two numbers
+                    10 + 10  = 20
+
+                concatenation: when there is at least one string
+                    "10" + "10" = "1010"
+                    1 + "10"  = "110"
+                    "2" + 3 = 23
+         */
+
+        String word = "C#";
+
+        System.out.println("My favorite language is " + word + " programming language");
+
+
+
+    }
+
+
+}
