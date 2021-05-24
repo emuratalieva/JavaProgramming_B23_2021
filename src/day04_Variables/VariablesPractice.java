7ab3bf9724e0f875e88a117c0a3041b1bb1dfa47
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-24 11:10:46 -0400

    day04

---
+++ src/day04_Variables/VariablesPractice.java
@@ -0,0 +1,43 @@
+package day04_Variables;
+
+public class VariablesPractice {
+
+    public static void main(String[] args) {
+        //    int num1 = 100, num2 = 10000, num6 = 850000;
+
+        int num1 = 100;
+        int num2 = 10000;
+        double num3 = 3.5;
+        double num4 = 2.5f;
+        long num5 = 999999999999L;
+        int num6 = 850000;
+
+      //  float num7 = 5.5;  larger primitives cannot be assigned to smaller primitives directly
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        System.out.println("num3 = " + num3);
+        System.out.println("num4 = " + num4);
+        System.out.println("num5 = " + num5);
+        System.out.println("num6 = " + num6);
+
+    }
+
+
+}
+
+/*
+
+1. create a class named Varibales_Practice & declear the following variables:
+                            num1 = 100;
+                            num2 = 10000;
+                            num3 = 3.5;
+                            num4 = 2.5f;
+                            num5 = 999999999999;
+                            num6 = 850000;
+
+                print each of the variables above on the console
+
+      DataType variableName = Data
+
+ */
