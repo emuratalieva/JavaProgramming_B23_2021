2ca8fef20e88efc7f4677f5500626222dc4dfad1
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-24 16:34:46 -0400

    day04

--- src/day04_Variables/ConcatenationPractice.java
+++ src/day04_Variables/ConcatenationPractice.java
@@ -18,6 +18,33 @@ public class ConcatenationPractice {
 
         System.out.println("My favorite language is " + word + " programming language");
 
+        System.out.println("--------------------------------------------------");
+
+        int dollar = 10000;
+        double lira = dollar * 8.3;
+        double euro = dollar * 0.9;
+
+        System.out.println(dollar + " US dollars equal to " + lira +" Turkish liras");
+        System.out.println(dollar + " US dollars equal to " + euro +" euros" );
+
+        System.out.println("-----------------------------------------------");
+        int a =100;
+        int b = 200;
+        int addition = a + b;
+        int subtraction = a -b;
+        int multiplication = a*b;
+
+        // 10 + 20 = 30
+        System.out.println( a + " + " + b +" = " +  addition );
+
+
+        // 10 - 20 = -10
+        System.out.println(a + " - " + b +" = " + subtraction );
+
+
+        // 100 * 200 = 20000
+        System.out.println(a + " * " + b +" = " + multiplication );
+
 
 
     }
