28ad4cdf6908bfd2f3e4bef6899e9288a294ee4b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-24 12:52:29 -0400

    day04

--- src/day04_Variables/StringOfText.java
+++ src/day04_Variables/StringOfText.java
@@ -2,5 +2,38 @@ package day04_Variables;
 
 public class StringOfText {
 
+    /*
+    String: for string of text. ""
+
+    int: Integers
+    double: Decimals
+    char: single characters
+    boolean: True/False
+     */
+
+    public static void main(String[] args) {
+        // Muhtar
+        String name ="Muhtar";
+
+        System.out.println("Muhtar");
+        System.out.println(name);
+
+        String brand = "Toyota";
+        String cityName = "McLean";
+        String gender = "Male";
+        char gender2 = 'M';
+
+        String s1 = "12";
+        int n1 = 12;
+
+        System.out.println(n1 * 2);
+     //   System.out.println(s1 * 2);
+
+
+
+    }
+
+
+
 
 }
