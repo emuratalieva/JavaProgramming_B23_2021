2ad1d305e432e450a6a715f28b16d9a0b79484ae
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-25 12:04:44 -0400

    day05

---
+++ src/day05_ArithmeticOperators/DivideTwoNumbers.java
@@ -0,0 +1,37 @@
+package day05_ArithmeticOperators;
+
+public class DivideTwoNumbers {
+
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 9;
+
+        int result = a/b;
+        int remainder = a%b;
+
+        System.out.println( a +" divide by "+b + " is equal to " +result
+         +" with a remainder of "+remainder);
+
+
+
+
+    }
+
+
+}
+/*
+1.
+		num1 = 10;
+		num2 = 3;
+
+
+		output:
+			10 divide by 3 is equal to 3 with a remainder of 1
+
+		num1 = 20
+		num2 = 9
+
+		output:
+			20 devide by 9 is equal to 2 with a remainder of 2
+
+ */
