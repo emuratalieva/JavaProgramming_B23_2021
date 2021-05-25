2d1749e62829afd3cd519c5bc6715d61cdeb42d0
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-25 11:59:09 -0400

    day05

---
+++ src/day05_ArithmeticOperators/ArithmeticOperators.java
@@ -0,0 +1,38 @@
+package day05_ArithmeticOperators;
+
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+        System.out.println( 25 / 2 ); //12
+
+        System.out.println( 25.0 / 2 );
+        System.out.println(25 / 2.0 );
+        System.out.println( 25d / 2);
+
+      //  System.out.println( 10 / 0 );
+
+        int a =  100/3 ; //33
+        System.out.println("a = " + a);
+
+        double b = 100/3;  // 33.3333..   or 33  or 33.0
+        //      b =   33 ==> 33.0
+
+        System.out.println("b = " + b);
+        
+        double c = 100.0/3;
+
+        System.out.println("c = " + c);
+
+        System.out.println("-------------------------------------------------");
+        int n1 = 100;
+        int n2 = 40;
+        // 100/40 ==>2
+        System.out.println(n1%n2);  // 20
+
+
+        
+        
+
+
+    }
+}
