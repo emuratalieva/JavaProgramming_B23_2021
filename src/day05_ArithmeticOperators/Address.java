19741f2e2672d5de9f07d90028ae66c24d63cc6c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-25 10:43:26 -0400

    day05

---
+++ src/day05_ArithmeticOperators/Address.java
@@ -0,0 +1,34 @@
+package day05_ArithmeticOperators;
+
+public class Address {
+
+    public static void main(String[] args) {
+        String name = "Conor",
+               buildingNumber = "123",
+               streetName = "Legacy Circle",
+               city = "Herndon",
+               state = "VA",
+               zipCode = "20171";
+
+        System.out.println(name + "\n" + buildingNumber +" " + streetName
+                +"\n"+ city+" " + state+" "+zipCode );
+
+
+
+
+
+    }
+
+}
+
+/*
+1. create a class named Address and declare the following variables:
+			name, buildingNumber, streetName, city, state, zipCode
+
+			use string concatenation to display the full address of the person
+
+			Ex:
+				Daniel Owens
+				7925 Jones Branch Dr
+				McLean VA 22102
+ */
