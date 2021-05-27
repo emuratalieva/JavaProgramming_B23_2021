7641a47c1a9198a2f34e4031ffd0ba6d5ca90b6f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-27 11:56:56 -0400

    day06

---
+++ src/day06_UnaryOperators/UnaryOperators.java
@@ -0,0 +1,28 @@
+package day06_UnaryOperators;
+
+public class UnaryOperators {
+
+    public static void main(String[] args) {
+        int a = 100;
+        ++a;
+
+        System.out.println("a = " + a);
+
+
+        int b = 100;
+        --b;
+
+        System.out.println("b = " + b);
+
+        System.out.println("-----------------------------------");
+        int x= 10;
+
+        System.out.println( ++x); // 11 pre:
+
+
+
+    }
+
+
+
+}
