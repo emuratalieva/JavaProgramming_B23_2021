02b55868995257c6bc6f389afcd10cba849de832
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-27 10:12:17 -0400

    day06

---
+++ src/day06_UnaryOperators/FlightTicket.java
@@ -0,0 +1,27 @@
+package day06_UnaryOperators;
+
+public class FlightTicket {
+
+    public static void main(String[] args) {
+        String from= "McLean",
+                to = "Lav Vegas";
+
+        double price = 425;
+
+        System.out.println("From "+from+" to "+to +" is $"+price);
+
+
+    }
+
+
+}
+
+/*
+1. create a class named FlightTicket, and declare the following variables:
+			 from, to, ticketPrice
+
+			 use concatenation to display the full info of the ticket
+
+			 ex:
+			 	 From Las Vegas to McLean is $425.5
+ */
