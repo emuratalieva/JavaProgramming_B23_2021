548ecb2804463d698ae860e4bb265c242b04f473
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-28 12:13:22 -0400

    day07

--- src/day07_ShortHand_Relational/RelationalOperators.java
+++ src/day07_ShortHand_Relational/RelationalOperators.java
@@ -1,4 +1,36 @@
 package day07_ShortHand_Relational;
 
 public class RelationalOperators {
+
+    public static void main(String[] args) {
+        // > ,  >=
+        System.out.println(  10 > 11); // false
+        System.out.println( 10 >= 10); // true
+
+        System.out.println(11 >= 9 ); // true
+        System.out.println( 20 >= 200); // false
+
+        // < , <=
+        System.out.println(1000 < 100);  // false
+        System.out.println( 1000 < 10000); // true
+
+        System.out.println(  10000 <= 1000  ); // false
+        System.out.println(1000 <= 10000);  // true
+
+        // ==,  !=
+        System.out.println(5 == 6); // false
+        System.out.println( 5 != 6 ); //true
+
+        System.out.println('a' == 'A'); // false
+        System.out.println( 'a' != 'A'); // true
+
+        System.out.println("Java"   == "java" ); //false: not equal
+        System.out.println( "Java" != "java");// true: not equal
+
+
+
+
+
+    }
+
 }
