79e47fefc9e9dd30217e60eba358f1dda830cd78
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-28 10:21:51 -0400

    day07

---
+++ src/day07_ShortHand_Relational/SwipeTwoNumbers.java
@@ -0,0 +1,30 @@
+package day07_ShortHand_Relational;
+
+public class SwipeTwoNumbers {
+
+    public static void main(String[] args) {
+        int a = 200;
+        int b = 300;
+
+        int c = a; // c= 200;
+
+        a = b; // a = 300;
+        b = c; // b = 300;
+
+        System.out.println("a = " + a); // 300
+        System.out.println("b = " + b); // 200
+
+    }
+
+}
+
+
+/*
+1. write a program that can swipe two variables' value by using a temporary variable
+			ex:
+				if a= 10, b=15
+
+				output:
+					a = 15
+					b = 10
+ */
