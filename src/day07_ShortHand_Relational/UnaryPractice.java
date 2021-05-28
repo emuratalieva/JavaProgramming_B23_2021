0cf4f19ddd9ae0eee3ccc1a3faa5d2869c6a5675
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-28 10:41:04 -0400

    day7

--- src/day07_ShortHand_Relational/UnaryPractice.java
+++ src/day07_ShortHand_Relational/UnaryPractice.java
@@ -4,6 +4,7 @@ public class UnaryPractice {
 
     public static void main(String[] args) {
         int a = 100; //a=100+1=101+1=102-1=101-1=100
+        
         int b = -a++ +  ++a  - a-- * a-- % 2;
         //  b = -100 +  102  - 102 * 101 % 2
         //  b = -100 + 102 - 10302 % 2
@@ -13,6 +14,11 @@ public class UnaryPractice {
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
+        
+        int c = 10;
+        System.out.println(c--); //10, c=9
+        System.out.println("c = " + c);
+        
 
     }
 
