b394df5d0c7917026bbac278b9e1721504db017f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-28 14:56:11 -0400

    day03

---
+++ src/day07_ShortHand_Relational/EligibleToBuyAlcohol.java
@@ -0,0 +1,14 @@
+package day07_ShortHand_Relational;
+
+public class EligibleToBuyAlcohol {
+
+    public static void main(String[] args) {
+        String name = "Sabir";
+        int age = 23;
+        boolean isEligible =   age >= 21;
+
+        System.out.println(name+" is eligible to buy alcohol: " +isEligible);
+
+
+    }
+}
