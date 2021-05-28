767615af060c7b38213b8216b4aac227f923ea8d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-28 10:33:34 -0400

    day07

---
+++ src/day07_ShortHand_Relational/UnaryPractice.java
@@ -0,0 +1,26 @@
+package day07_ShortHand_Relational;
+
+public class UnaryPractice {
+
+    public static void main(String[] args) {
+        int a = 100; //a=100+1=101+1=102-1=101-1=100
+        int b = -a++ +  ++a  - a-- * a-- % 2;
+        //  b = -100 +  102  - 102 * 101 % 2
+        //  b = -100 + 102 - 10302 % 2
+        //  b = -100 + 102 - 0
+        //  b = 2;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+    }
+
+}
+
+/*
+2. manually calculate the following code fragements:
+
+                1. int a = 100;
+                   int b = -a++ + ++a - a-- * a-- % 2
+ */
