e4362185ea9b7194d7d8cca1ebb5989e8ac83533
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-28 15:53:53 -0400

    day07

---
+++ src/day07_ShortHand_Relational/PassOrFail.java
@@ -0,0 +1,21 @@
+package day07_ShortHand_Relational;
+
+public class PassOrFail {
+
+    public static void main(String[] args) {
+
+        int score = 59;
+
+        boolean pass = score>= 60;
+        //              59 >= 60
+        //                false
+
+        System.out.println("Passed the exam: "+pass);
+
+
+        // 60
+
+
+    }
+
+}
