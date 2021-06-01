0db9b956fe19b323e497f144dfa54416442c4739
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-01 10:43:45 -0400

    day08

---
+++ src/day08_ifStatements/LogicalOperators.java
@@ -0,0 +1,38 @@
+package day08_ifStatements;
+
+public class LogicalOperators {
+
+    public static void main(String[] args) {
+
+        System.out.println(  !true ); // false
+        System.out.println( !false ); // true
+
+        System.out.println( true == !false ); // true
+        //                  true == true
+
+        System.out.println( true != !true); //
+        //                  true != false
+
+        System.out.println("--------------------------------");
+
+        int score = 80;
+        boolean passed =  score >= 60;
+        boolean failed =  !passed ;
+      //  boolean failed = score < 60;
+
+
+
+        System.out.println("----------------------------------");
+
+        int age = 21;
+
+        boolean eligible =   age >= 21;
+        boolean notEligible = !eligible ;
+
+
+
+
+    }
+
+
+}
