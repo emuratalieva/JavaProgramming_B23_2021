7d3a3d03e6dc810ec95ccef3127ef9366e8fd242
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-01 12:08:13 -0400

    day08

---
+++ src/day08_ifStatements/OddOrEven.java
@@ -0,0 +1,23 @@
+package day08_ifStatements;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+
+        int number =  100;
+
+        boolean odd = number%2 != 0 ;
+
+        if (odd) { // if the number is odd number
+            System.out.println(number+" is odd number");
+        }
+
+        if(!odd){ // if number is not odd number
+            System.out.println(number+" is even number");
+        }
+
+
+
+    }
+
+}
