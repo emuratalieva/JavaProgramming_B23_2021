06b02dfb8567525e4ab98cf5ecb8ba9027a0be3e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-01 12:01:49 -0400

    day08

--- src/day08_ifStatements/PosNegZero.java
+++ src/day08_ifStatements/PosNegZero.java
@@ -2,6 +2,28 @@ package day08_ifStatements;
 
 public class PosNegZero {
 
+    public static void main(String[] args) {
+        int number = 10000;
 
-    
+        boolean positive = number > 0;
+        boolean negative = number < 0;
+        boolean zero =  !positive && !negative ;
+
+        if(positive){  //if the number is positive:
+            System.out.println(number+" is positive");
+        }
+
+        if (negative){ // if the number is negative:
+            System.out.println(number +" is negative");
+        }
+
+        if(zero){ // if the number is zero:
+            System.out.println(number+" is zero");
+        }
+
+
+
+    }
+
+
 }
