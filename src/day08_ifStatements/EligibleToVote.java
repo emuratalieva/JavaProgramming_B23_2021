5b9e4a9fef7dcfe08dc83a4e1aff3e8f7122ce9c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-01 12:15:02 -0400

    day08

---
+++ src/day08_ifStatements/EligibleToVote.java
@@ -0,0 +1,24 @@
+package day08_ifStatements;
+
+public class EligibleToVote {
+
+    public static void main(String[] args) {
+        String name = "James";
+        int age =15;
+        String citizen = "USA";
+
+        boolean eligibleToVote =  age >= 21  && citizen == "USA";
+
+        if(eligibleToVote){ // if the person is eligible
+            System.out.println(name+" is eligible to vote");
+        }
+
+
+        if(!eligibleToVote){  // if the person is NOT eligible
+            System.out.println(name+" is not eligible to vote");
+        }
+
+
+    }
+
+}
