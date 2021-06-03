4ced872bc59b2f20a02df69fe75e167c592ec35b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-03 10:17:18 -0400

    day09

---
+++ src/day09_IfStatements/MinimumNumber.java
@@ -0,0 +1,30 @@
+package day09_IfStatements;
+
+public class MinimumNumber {
+
+    public static void main(String[] args) {
+        int num1 =20, num2 = 30;
+
+        if(num1 < num2){
+            System.out.println(num1+" is minimum");
+        }
+
+        if(num2 < num1){
+            System.out.println(num2+" is minimum");
+        }
+
+
+    }
+
+}
+
+/*
+ 2. write a program that can print out the minimum number between two numbers
+
+        Ex:
+            num1 = 20;
+            num2 = 30;
+
+            output:
+                20 is the minimum number
+ */
