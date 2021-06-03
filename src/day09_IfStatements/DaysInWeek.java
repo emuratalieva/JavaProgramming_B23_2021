1c91155409af3bfe2c3049eefd77233eba2d438a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-03 11:48:27 -0400

    day09

---
+++ src/day09_IfStatements/DaysInWeek.java
@@ -0,0 +1,41 @@
+package day09_IfStatements;
+
+public class DaysInWeek {
+
+    public static void main(String[] args) {
+        int number = 1;
+
+        if(number == 1){
+            System.out.println("Monday");
+        }
+
+        if (number == 2){
+            System.out.println("Tuesday");
+        }
+
+        if (number == 3){
+            System.out.println("Wednesday");
+        }
+
+        if (number == 4){
+            System.out.println("Thursday");
+        }
+
+        if (number == 5){
+            System.out.println("Friday");
+        }
+
+        if (number == 6){
+            System.out.println("Saturday");
+        }
+
+        if (number == 7){
+            System.out.println("Sunday");
+        }
+
+
+
+
+
+    }
+}
