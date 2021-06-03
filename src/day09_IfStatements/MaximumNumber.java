4434ab54880608968043f97ae59551616c0f1512
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-03 10:13:03 -0400

    day09

---
+++ src/day09_IfStatements/MaximumNumber.java
@@ -0,0 +1,29 @@
+package day09_IfStatements;
+
+public class MaximumNumber {
+
+    public static void main(String[] args) {
+        int num1 = 20,
+                num2 = 30;
+
+        if(num1 > num2){
+            System.out.println(num1+" is maximum");
+        }
+
+        if(num2 > num1){
+            System.out.println(num2+" is maximum");
+        }
+
+
+    }
+
+}
+/*
+1. write a program that can print out the maximum number between two different numbers
+	        Ex:
+	            num1 = 20;
+	            num2 = 30;
+
+            output:
+	                30 is the maximum number
+ */
