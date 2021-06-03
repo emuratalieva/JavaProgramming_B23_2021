67063f962f3b92e4fd2115ddaea04a741b27cd4d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-03 11:00:42 -0400

    day09

---
+++ src/day09_IfStatements/EligibleToBuyAlcohol.java
@@ -0,0 +1,17 @@
+package day09_IfStatements;
+
+public class EligibleToBuyAlcohol {
+
+    public static void main(String[] args) {
+        int age = 30;
+        
+        if(age >= 21){
+            System.out.println("Eligible");
+        }else{  // otherwise: age < 21
+            System.out.println("Not Eligible");
+        }
+
+
+    }
+
+}
