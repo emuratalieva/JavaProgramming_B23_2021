2e3ac81b873a1344324ef26b69c27280bbfaf5cd
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-03 10:56:50 -0400

    day09

---
+++ src/day09_IfStatements/OddOrEven.java
@@ -0,0 +1,19 @@
+package day09_IfStatements;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+        int number = 100;
+
+        if(number %2 != 0){
+            System.out.println(number+" is odd number");
+        }else{
+            System.out.println(number+" is even number");
+        }
+
+
+
+    }
+
+
+}
