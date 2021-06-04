fa70c55f9ee30b0c9c9b45fe72e361ff394dfaf4
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-04 10:39:54 -0400

    day10

---
+++ src/day10_IfStatements/NameOfMonth.java
@@ -0,0 +1,42 @@
+package day10_IfStatements;
+
+public class NameOfMonth {
+
+    public static void main(String[] args) {
+        int number = 10;  // 1 ~ 12
+        String name = "";
+
+        if(number == 1){
+            name = "Jan" ;
+        }else if(number == 2){
+            name = "Feb";
+        }else if(number == 3){
+           name = "Mar";
+        }else if(number == 4){
+            name = "Apr";
+        }else if(number == 5){
+            name = "May";
+        }else if(number == 6){
+            name = "Jun";
+        }else if(number == 7){
+            name = "Jul";
+        }else if(number == 8){
+            name = "Aug";
+        }else if(number == 9){
+            name = "Sep";
+        }else if(number == 10){
+            name = "Oct";
+        }else if(number == 11){
+            name = "Nov";
+        }else {
+            name = "Dec";
+        }
+
+        System.out.println("name = " + name);
+
+
+
+
+    }
+
+}
