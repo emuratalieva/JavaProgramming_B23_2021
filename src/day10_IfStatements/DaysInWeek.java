2a80eb4a286ca153a0dda46a341a551bff156801
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-04 12:13:31 -0400

    day10

--- src/day10_IfStatements/DaysInWeek.java
+++ src/day10_IfStatements/DaysInWeek.java
@@ -3,29 +3,33 @@ package day10_IfStatements;
 public class DaysInWeek {
 
     public static void main(String[] args) {
-        int number = 5 ;  // 1 ~ 7
+        int number = 5;
         String name = "";
 
+        if (number >= 1 && number <= 7) {
 
-        if(number == 1){
-            name = "Monday";
-        }else if(number == 2){
-            name = "Tuesday";
-        }else if(number == 3){
-            name = "Wednesday";
-        }else if(number ==4){
-            name = "Thursday";
-        }else if(number == 5){
-            name = "Friday";
-        }else if(number == 6){
-            name = "Saturday";
-        }else {
-            name = "Sunday";
+            if (number == 1) {
+                name = "Monday";
+            } else if (number == 2) {
+                name = "Tuesday";
+            } else if (number == 3) {
+                name = "Wednesday";
+            } else if (number == 4) {
+                name = "Thursday";
+            } else if (number == 5) {
+                name = "Friday";
+            } else if (number == 6) {
+                name = "Saturday";
+            } else {
+                name = "Sunday";
+            }
+
+        } else {
+            name = "No Such A Day";
         }
 
+
         System.out.println("name = " + name);
-
-
 
 
     }
