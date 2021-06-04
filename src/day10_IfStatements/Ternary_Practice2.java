7e59fb30ee8f8153b62210704c12e84327a1480d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-04 15:54:54 -0400

    day10

--- src/day10_IfStatements/Ternary_Practice2.java
+++ src/day10_IfStatements/Ternary_Practice2.java
@@ -19,7 +19,23 @@ public class Ternary_Practice2 {
         }
          */
 
-        String result =
+        String result = (number > 0) ? "Positive" :(number < 0)? "Negative" : "Zero";
+
+        System.out.println("------------------------------------------------------------");
+        int score  = 100;
+
+        char grade = ' ';
+        if(score >= 90){
+            grade = 'A';
+        }else if(score >= 80){
+            grade = 'B';
+        }else if(score >= 70){
+            grade = 'C';
+        }else if(score >= 60){
+            grade = 'D';
+        }else{
+            grade = 'F';
+        }
 
 
 
