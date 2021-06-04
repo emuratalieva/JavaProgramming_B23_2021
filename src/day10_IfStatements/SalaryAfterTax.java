5bd89716f5bd93850971e099be6d7bbb8a5d8ccd
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-04 11:08:18 -0400

    day10

---
+++ src/day10_IfStatements/SalaryAfterTax.java
@@ -0,0 +1,46 @@
+package day10_IfStatements;
+
+public class SalaryAfterTax {
+
+    public static void main(String[] args) {
+        double salary = 100_000;
+
+        double taxRate = 0;
+
+        if(salary>= 130000 ){ // false: salary < 130000
+            taxRate = 0.35;
+        }else if(salary>= 100000 ){ // false: salary < 100000
+            taxRate = 0.30;
+        }else if(salary>= 80000 ){ // false: salary < 80000
+            taxRate = 0.25;
+        }else { // salary < 80000
+            taxRate = 0.20;
+        }
+
+        boolean isMarried = false;
+
+        if (isMarried){
+            taxRate -= 0.05;
+        }
+
+        double salaryAfterTax = salary - (salary*taxRate);
+
+        System.out.println("Salary After Tax = " + salaryAfterTax);
+
+
+
+    }
+
+
+}
+
+/*
+2. Write a program that can calculate the salary after tax based on the following requirments.
+				the tax rates are:
+						35% for salary of 130K or more
+						30% for salary of 100K to 130k
+						25% for salary of 80K to 100K
+						20% for salary of 79K or less
+
+				in addition,if the person is married, he/she will pay 5% less tax
+ */
