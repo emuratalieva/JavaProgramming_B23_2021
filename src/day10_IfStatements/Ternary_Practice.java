403f0f8479d0a5775f3cc51b13db8df43d4d6563
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-04 15:45:27 -0400

    day10

---
+++ src/day10_IfStatements/Ternary_Practice.java
@@ -0,0 +1,29 @@
+package day10_IfStatements;
+
+public class Ternary_Practice {
+
+    public static void main(String[] args) {
+        int age = 23;
+        String result =  (age >= 21) ? "Eligible" : "Not eligible"   ;
+        /*
+        if(age >= 21){
+             result ="Eligible";
+        }else{
+            result = "Not eligible";
+        }
+         */
+
+        System.out.println(result);
+
+        int score = 80;
+
+        System.out.println(    (score>=60) ? "Passed" : "Failed"    );
+        
+
+
+
+
+    }
+
+
+}
