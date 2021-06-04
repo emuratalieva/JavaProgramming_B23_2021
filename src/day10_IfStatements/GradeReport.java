91f7bd0575dc4205744f1bf08d3e3cce35e50707
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-04 12:05:44 -0400

    day08

--- src/day10_IfStatements/GradeReport.java
+++ src/day10_IfStatements/GradeReport.java
@@ -3,8 +3,29 @@ package day10_IfStatements;
 public class GradeReport {
 
     public static void main(String[] args) {
-        int score = 85;
-        String grade = "";
+        int score = 135;  // 0 ~ 100
+        String grade = ""; // A, B, C, D, F
+
+        if(score >= 0 && score<= 100){ // pre condition: is the score is valid
+
+            if(score>= 90){
+                grade = "A";
+            }else if(score >= 80){
+                grade = "B";
+            }else if(score >= 70){
+                grade = "C";
+            }else if(score>=60){
+                grade = "D";
+            }else{
+                grade = "F";
+            }
+
+        }else{ // opposite of pre condition: if the score is invalid
+            grade = "Invalid";
+        }
+
+        System.out.println("grade = " + grade);
+
 
 
     }
