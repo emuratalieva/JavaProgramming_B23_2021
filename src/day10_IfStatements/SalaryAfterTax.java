b55a1b5a269ecb4e0952403e647358966a900fb6
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-04 11:48:39 -0400

    day10

--- src/day10_IfStatements/SalaryAfterTax.java
+++ src/day10_IfStatements/SalaryAfterTax.java
@@ -3,7 +3,8 @@ package day10_IfStatements;
 public class SalaryAfterTax {
 
     public static void main(String[] args) {
-        double salary = 100_000;
+        double salary = 75_000;
+        boolean isMarried = true;
 
         double taxRate = 0;
 
@@ -16,12 +17,14 @@ public class SalaryAfterTax {
         }else { // salary < 80000
             taxRate = 0.20;
         }
+        // tax = 20%
 
-        boolean isMarried = false;
 
-        if (isMarried){
-            taxRate -= 0.05;
+        if (isMarried){ // if person is married
+            taxRate -= 0.05; // tax rate will be subtracted by 5
         }
+        // tax: 15
+
 
         double salaryAfterTax = salary - (salary*taxRate);
 
