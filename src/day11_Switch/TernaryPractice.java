e8c4f576d08816d2edfe5b3a305c2def2fc0e65d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-07 11:04:09 -0400

    day11

--- src/day11_Switch/TernaryPractice.java
+++ src/day11_Switch/TernaryPractice.java
@@ -24,7 +24,6 @@ public class TernaryPractice {
         }
          */
 
-
         System.out.println("result = " + result);
 
         System.out.println("------------------------------------------------------------------");
@@ -41,6 +40,42 @@ public class TernaryPractice {
         int price = (sport == "Soccer" || sport == "Tennis" ) ? 100 : 50 ;
         System.out.println("price = " + price);
 
+        System.out.println("----------------------------------------------------------------");
+        /*
+        3. write a program that checks for the bigger of 3 numbers.
+		you get 3 int variables with different values: n1 , n2 and n3
+			if n1 is biggest output: "n1 is bigger"
+			if n2 is biggest output: "n2 is bigger"
+			if n3 is biggest output: "n3 is bigger"
+
+			NOTE: MUST APPLY TERNARY
+         */
+
+        int n1 = 100,
+                n2 = 500,
+                n3 = 300;
+
+        String result2 = (n1 > n2 && n1 > n3)?  n1+" is bigger"
+                :(n2 > n1 && n2 >n3)? n2+" is bigger"
+                : n3+" is bigger"  ;
+
+        System.out.println("result2 = " + result2);
+
+        System.out.println("-----------------------------------------------------------");
+        int number = 12;
+        String name = "";
+
+        if(number >= 1 && number<= 12){
+
+            name =   (number == 1)? "Jan" :(number==2)? "Feb" :(number == 3)? "Mar" :(number==4)?"Apr"
+                    :(number==5)?"May" :(number==6)?"Jun" :(number==7)? "Jul" :(number==8)? "Aug"
+                    :(number ==9)? "Sep" :(number==10)? "Oct" :(number==11)?"Nov" : "Dec";
+
+            //   11 ? and 11 :
+
+        }else{
+            name = "Invalid";
+        }
 
 
 
