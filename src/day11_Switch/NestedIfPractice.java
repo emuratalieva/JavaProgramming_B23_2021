5f3f08ed7bc4b6e5370e29eb5d717173b9ec01a3
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-07 12:11:03 -0400

    day11

--- src/day11_Switch/NestedIfPractice.java
+++ src/day11_Switch/NestedIfPractice.java
@@ -36,6 +36,44 @@ public class NestedIfPractice {
 
         System.out.println("result = " + result);
 
+        System.out.println("------------------------------------------");
+        /*
+        5. write a program that can define the age groups of a person
+            age groups are:
+                    Teenager (< 21)
+                    Adult   (>=21 && <55 )
+                    Senior  ( > 55 )
+
+             if age is negative or greater than 150, print invalid
+
+                  NOTE: MUST APPLY NESTED IF
+         */
+
+        int age = 45;
+        String ageGroup = "";
+
+        if( age >= 0 && age <= 150){
+            if(age < 21){
+                ageGroup = "Teenager";
+            }else if(age >= 21 && age < 55){
+                ageGroup = "Adult";
+            }else{
+                ageGroup = "Senior";
+            }
+        }else{
+            ageGroup = "Invalid";
+        }
+
+        System.out.println("ageGroup = " + ageGroup);
+
+
+
+
+
+
+
+
+
 
 
     }
