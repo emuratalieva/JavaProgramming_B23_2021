afb2e24bad03da01e0f14f3e7735baf14aaf579c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-07 15:55:38 -0400

    day11

---
+++ src/day11_Switch/Calculator.java
@@ -0,0 +1,22 @@
+package day11_Switch;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        double n1 =100;
+        double n2 = 2.5;
+
+        char mathOperator = '-';
+
+        switch ()
+
+
+
+
+
+
+
+    }
+
+
+}
