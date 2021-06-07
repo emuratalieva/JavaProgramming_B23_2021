fdaabe84f7503dec53e5bd24aae39cab0fc0c3b8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-07 16:04:29 -0400

    day11

--- src/day11_Switch/Calculator.java
+++ src/day11_Switch/Calculator.java
@@ -5,10 +5,33 @@ public class Calculator {
     public static void main(String[] args) {
         double n1 =100;
         double n2 = 2.5;
+        double result =0 ;
+        char mathOperator = '*';  // +, -, *, /
 
-        char mathOperator = '-';
+        switch (mathOperator){
 
-        switch ()
+            case '-':
+                result = n1-n2;
+                break;
+
+            case '+':
+               result = n1+n2;
+                break;
+
+            case '*':
+                result = n1*n2;
+                break;
+
+            case '/':
+                result = n1/n2;
+                break;
+
+            default:
+                System.out.println("Invalid Operator");
+        }
+
+
+        System.out.println("result = " + result);
 
 
 
