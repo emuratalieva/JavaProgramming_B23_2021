bc3e74b11196f20e6db3cfd1cfbe570d25759f2a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-08 11:57:18 -0400

    day12

---
+++ src/day12_Scanner/ScannerIntro.java
@@ -0,0 +1,31 @@
+package day12_Scanner;
+
+import java.util.Scanner;
+
+public class ScannerIntro {
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        int addition = num1 + num2;
+        int subtraction = num1 - num2;
+        int multiplication = num1 * num2;
+        int division = num1/num2;
+
+        System.out.println("addition = " + addition);
+        System.out.println("subtraction = " + subtraction);
+        System.out.println("multiplication = " + multiplication);
+        System.out.println("division = " + division);
+
+
+
+
+    }
+
+
+}
