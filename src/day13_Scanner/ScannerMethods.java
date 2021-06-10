e97c4eb56cb05b2acb22570401aeb51f0f91bd06
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-10 11:31:14 -0400

    day13

---
+++ src/day13_Scanner/ScannerMethods.java
@@ -0,0 +1,38 @@
+package day13_Scanner;
+
+import java.util.Scanner;
+
+public class ScannerMethods {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("You are full time employee:");
+        boolean isFullTime = scan.nextBoolean();
+
+        System.out.println("You are an adult:");
+        boolean isAdult = scan.nextBoolean();
+
+        System.out.println("You are married:");
+        boolean isMarried = scan.nextBoolean();
+
+        System.out.println("isFullTime = " + isFullTime);
+        System.out.println("isAdult = " + isAdult);
+        System.out.println("isMarried = " + isMarried);
+
+
+        /*
+        FreeShipping:
+            price
+            isPrime:
+                true: free shipping
+                false: 3.99 shipping fee
+
+         */
+
+
+
+
+    }
+
+}
