876ff0abeef35ed516e0960dd7b7694a57665a9d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-10 12:09:17 -0400

    day13

--- src/day13_Scanner/NextLineMethod.java
+++ src/day13_Scanner/NextLineMethod.java
@@ -6,7 +6,26 @@ public class NextLineMethod {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        
+        System.out.println("Enter your job title:");
+        String jobTitle = scan.nextLine();
+
+        System.out.println("jobTitle = " + jobTitle);
+
+
+        /*
+        System.out.println("Enter your job tile:");
+        String jobTitle = scan.next(); // Software
+
+        scan.next(); // Developer
+
+        System.out.println("Enter your gender:");
+        String gender = scan.next();
+
+        System.out.println("jobTitle = " + jobTitle);
+        System.out.println("gender = " + gender);
+
+         */
+
 
 
     }
