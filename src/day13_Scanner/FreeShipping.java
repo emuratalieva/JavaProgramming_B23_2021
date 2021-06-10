caca131e220bf7d56d24e74e56b8a8602900c43a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-10 11:38:11 -0400

    day13

---
+++ src/day13_Scanner/FreeShipping.java
@@ -0,0 +1,24 @@
+package day13_Scanner;
+
+import java.util.Scanner;
+
+public class FreeShipping {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the price of the item:");
+        double price = scan.nextDouble();
+
+        System.out.println("Are you a prime member?");
+        boolean isPrime = scan.nextBoolean();
+
+        double totalPrice =  (isPrime)? price  :(price>=25)? price : price + 3.99;
+
+        System.out.println("total Price = $" + totalPrice);
+
+
+
+    }
+
+}
