cc16a41ea9a0a1357cbaa7cbe5fec137c23d67d8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-11 11:24:54 -0400

    day14

---
+++ src/day14_Recap/LetterOrDigitOrSpecialChar.java
@@ -0,0 +1,36 @@
+package day14_Recap;
+
+public class LetterOrDigitOrSpecialChar {
+
+    public static void main(String[] args) {
+
+        char ch = ')';
+
+        boolean isDigit = ch >= '0' && ch <= '9';
+
+        boolean isLetter =  ( ch >= 'A' && ch <= 'Z')  ||  (ch >='a' && ch <= 'z') ;
+                        // if the character is upper case letter or lower case letter
+
+        // boolean isSpecialCharacter = !isDigit && !isLetter;
+        /*
+        String result = "";
+        if(isDigit){
+            result =  ch+ " is digit";
+        }else if (isLetter){
+            result = ch+ " is letter";
+        }else{
+            result = ch+" is special character";
+        }
+         */
+
+        String result =  (isDigit) ?  ch+ " is digit" :(isLetter)? ch+ " is letter" : ch+" is special character";
+
+        System.out.println( result);
+
+
+
+
+    }
+
+
+}
