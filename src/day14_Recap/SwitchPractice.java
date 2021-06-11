726682d04ae894d1f2b18d0b39375a7a56ca27dd
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-11 15:14:55 -0400

    day14

--- src/day14_Recap/SwitchPractice.java
+++ src/day14_Recap/SwitchPractice.java
@@ -1,8 +1,43 @@
 package day14_Recap;
 
+import java.util.Scanner;
+
 public class SwitchPractice {
 
     public static void main(String[] args) {
+
+        Scanner scan =new Scanner(System.in);
+        System.out.println("How many people do you live with?");
+        int numberOfPeople = scan.nextInt();
+
+        if(numberOfPeople >= 1){
+
+            switch ( numberOfPeople ){
+                case 1:
+                case 2:
+                    System.out.println("Live with less than 3 people");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                    System.out.println("Live with 3 - 6 people");
+                    break;
+
+                default:
+                    System.out.println("Live with more than 6 people");
+            }
+
+        }else{
+            System.out.println("Invalid Number");
+        }
+
+
+
+        
+
+
 
     }
 
@@ -13,6 +48,6 @@ public class SwitchPractice {
         Ask the user how many people they live with?
             if user lives with Less than 3 people: print "Live with less than 3 people"
             if the user lives with 3 - 6 people: print "Live with 3 - 6 people"
-            if the user lives with more than 6 people: print "Live with "more than 6 people"
+            if the user lives with more than 6 people: print "Live with more than 6 people"
 
  */
