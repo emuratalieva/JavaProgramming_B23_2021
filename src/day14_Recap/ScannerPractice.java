e636108e322a4a2d153e1cd44aedbb74cd0874b2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-11 16:21:32 -0400

    day14

--- src/day14_Recap/ScannerPractice.java
+++ src/day14_Recap/ScannerPractice.java
@@ -33,14 +33,4 @@ public class ScannerPractice {
 /*
  - - Ask the user to enter the year, month number, and day they were born and print in the following format:
                 "month / day / year is your birthday "
-
-
-- - Ask the user to enter their zipcode (long), the number of people they live with (byte), if they are married or not (boolean - true for married/false for not) and print the values in the following format:
-
-    Ex:
-        inputs: 31009, 4, false
-
-        output:
-             I see you are not married and live with 4 people in your house which is in the 31009 area!
-
  */
---
+++ src/day14_Recap/ScannerPractice2.java
@@ -0,0 +1,40 @@
+package day14_Recap;
+
+import java.util.Scanner;
+
+public class ScannerPractice2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your zip code:");
+        long zipCode = scan.nextLong();
+
+        System.out.println("How many people do you live with?");
+        byte numberOfPeople = scan.nextByte();
+
+        System.out.println("Are you married?");
+        boolean isMarried = scan.nextBoolean();
+
+        System.out.println("I see you are " +  ( (isMarried)?"married":"not Married" )
+                +" and live with "+numberOfPeople +" people in your house which is in the "+zipCode+" area!"
+            );
+
+
+
+
+    }
+
+}
+
+/*
+- Ask the user to enter their zipcode (long), the number of people they live with (byte),
+if they are married or not (boolean - true for married/false for not) and print the values in the following format:
+
+    Ex:
+        inputs: 31009, 4, false
+
+        output:
+             I see you are not married and live with 4 people in your house which is in the 31009 area!
+
+ */
