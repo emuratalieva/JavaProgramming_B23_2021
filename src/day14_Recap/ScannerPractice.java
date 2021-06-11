7df252f65583cc32908cff30d0f22845a139bbb1
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-11 16:07:47 -0400

    day14

---
+++ src/day14_Recap/ScannerPractice.java
@@ -0,0 +1,46 @@
+package day14_Recap;
+
+import java.util.Scanner;
+
+public class ScannerPractice {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your birth year:");
+        short year = scan.nextShort();
+
+        System.out.println("Enter your birth month:");
+        byte month = scan.nextByte();
+
+        System.out.println("Enter your birth day:");
+        byte day = scan.nextByte();
+
+        boolean isValid = (year>0 && year<2021) && (month>= 1 && month<= 12) && (day>= 1 && day <= 31);
+
+        if(isValid){
+            System.out.println(month+"/"+day+"/"+year +" is your birthday!");
+        }else{
+            System.err.println("Invalid Entry");
+        }
+
+    scan.close();
+
+    }
+
+
+}
+
+/*
+ - - Ask the user to enter the year, month number, and day they were born and print in the following format:
+                "month / day / year is your birthday "
+
+
+- - Ask the user to enter their zipcode (long), the number of people they live with (byte), if they are married or not (boolean - true for married/false for not) and print the values in the following format:
+
+    Ex:
+        inputs: 31009, 4, false
+
+        output:
+             I see you are not married and live with 4 people in your house which is in the 31009 area!
+
+ */
