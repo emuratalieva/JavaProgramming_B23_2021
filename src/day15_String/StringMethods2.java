0673017a86185cc01d193d0b2d9e7b91cadbac4c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-14 15:42:37 -0400

    day15

--- src/day15_String/StringMethods2.java
+++ src/day15_String/StringMethods2.java
@@ -2,6 +2,37 @@ package day15_String;
 
 public class StringMethods2 {
 
+    public static void main(String[] args) {
+
+        String sentence = "Java Programming Language";
+        //index:           0123
+        String name = sentence.substring(0, 3+1);
+
+        System.out.println(name);
+
+        String s2 = "Cybertek School";
+        //           0123456789
+
+        String name2 = s2.substring(0, 8);
+        System.out.println(name2);
+
+        //String name3 = s2.substring(9, 15);
+        String name3 = s2.substring(9);
+
+
+        System.out.println("--------------------------------------------");
+        String sentence2 = "Java is a programming language";
+
+        String name4 = sentence2.substring(10);
+        System.out.println(name4);
+
+        String name5 = sentence2.substring(10, 20+1 );
+
+        System.out.println(name5);
+
+
+
+    }
 
 
 
