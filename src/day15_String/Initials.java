71a7dbed268de7038e48520124000f2284e18cbc
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-14 12:14:17 -0400

    day15

---
+++ src/day15_String/Initials.java
@@ -0,0 +1,45 @@
+package day15_String;
+
+import java.util.Scanner;
+
+public class Initials {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your first name:");
+        String firstName = scan.next();
+
+        System.out.println("Enter your last name:");
+        String lastName = scan.next();
+        scan.close();
+
+        char f = firstName.charAt(0);
+        char l = lastName.charAt(0);
+
+        String initial = f+"." +l;
+
+        System.out.println("initial = " + initial);
+
+
+
+    }
+
+
+}
+
+/*
+Ask user to enter:
+        1. first name
+        2. last name
+
+then print the initials
+
+    ex: Aaron
+        King
+
+        Initial: A.K
+
+
+ */
---
+++ src/day15_String/StringMethods.java
@@ -0,0 +1,20 @@
+package day15_String;
+
+public class StringMethods {
+
+    public static void main(String[] args) {
+
+        String str = "Muhtar";
+
+        char ch = str.charAt(0);
+
+        System.out.println("ch = " + ch);
+
+        System.out.println("-----------------------------------");
+
+
+
+
+    }
+
+}
