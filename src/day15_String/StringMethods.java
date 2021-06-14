0e5351ee5e335d71e02c8617c9427d4901d3c243
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-14 12:59:37 -0400

    day15

--- src/day15_String/StringMethods.java
+++ src/day15_String/StringMethods.java
@@ -6,11 +6,44 @@ public class StringMethods {
 
         String str = "Muhtar";
 
-        char ch = str.charAt(0);
+        char ch1 = str.charAt(0);
 
-        System.out.println("ch = " + ch);
+        System.out.println("ch1 = " + ch1);
+
+        String word = "Cybertek School";
+        //index:       0123456789...
+        char ch2 = word.charAt(9);
+        System.out.println("ch2 = " + ch2);
+
+        String word2 = "Java";
+        //index:        0123
+        char ch3 = word2.charAt(2);
+        System.out.println("ch3 = " + ch3);
 
         System.out.println("-----------------------------------");
+        String sentence = "Hello everyone, how are you all today?";
+        int totalNumberOfChars = sentence.length();
+        System.out.println("totalNumberOfChars = " + totalNumberOfChars);
+
+        // chars:    8
+        String s1 = "Cybertek";
+        //index:     01234567
+        int lastIndex = s1.length()-1; //in order to get the last index number we need to subtract one from the length of String
+        char firstCharacter = s1.charAt(0);
+       // char lastCharacter = s1.charAt(7);
+        char lastCharacter = s1.charAt( lastIndex  );
+
+        System.out.println("firstCharacter = " + firstCharacter);
+        System.out.println("lastCharacter = " + lastCharacter);
+
+        System.out.println("----------------------------------");
+        String firstName = "John";
+        String lastName = "King";
+
+        String fullName = firstName + " "  + lastName;
+     //   String fullName2 = firstName.concat(" ").concat(lastName);
+
+
 
 
 
