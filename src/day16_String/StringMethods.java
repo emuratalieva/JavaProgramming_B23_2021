7e3df09de2d8a4faf3c0836c5df47394bb10077b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-15 12:54:48 -0400

    day16

--- src/day16_String/StringMethods.java
+++ src/day16_String/StringMethods.java
@@ -37,6 +37,37 @@ public class StringMethods {
         System.out.println("indexOfFifthA = " + indexOfFifthA);
 
 
+        System.out.println("-----------------------------------");
+        String s2 ="       ";
+         s2 = s2.trim(); //""
+        boolean r1 = s2.isEmpty();
+        System.out.println("r1 = " + r1);
+
+        System.out.println("------------------------------------");
+        // equals(), or equalsIgnoreCase()
+
+        String str1 = "Cat"; // string pool
+        String str2 = new String("Cat"); // Java Heap
+        String str3 = new String("Cat"); // Java Heap
+
+        System.out.println(str1 == str2); // false
+        System.out.println( str1.equals( str2 ) ); // true
+
+        System.out.println( str2 == str3 ); // false
+        System.out.println( str2.equals(str3) ); //true
+
+        System.out.println("--------------------------------");
+        String word1 = "Java";
+        String word2 = "java";
+
+        System.out.println(word1 == word2);// false: two different object
+        System.out.println( word1.equals(word2) ); // false: case sensitivity
+
+        System.out.println( word1.equalsIgnoreCase(word2) );
+
+
+
+
 
 
 
