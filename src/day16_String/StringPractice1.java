c13d6157e59a5afd0600c8b73f6f76676224d693
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-15 10:35:18 -0400

    day16

---
+++ src/day16_String/StringPractice1.java
@@ -0,0 +1,43 @@
+package day16_String;
+
+import java.util.Scanner;
+
+public class StringPractice1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String word = scan.nextLine(); // "Programming"
+
+        /*
+        String result = ( word.length() == 0)? "String is empty"
+                        :(word.length() > 3)?  word.substring( word.length()-3 )
+                        : word;
+         */
+
+        String result = "";
+
+
+        if( word.length() == 0){
+            result = "String is empty";
+        }else if(word.length() > 3){
+            result =  word.substring( word.length()-3 )  ;
+        }else{
+            result = word;
+        }
+
+        System.out.println(result);
+
+        scan.close();
+
+    }
+
+}
+
+/*
+	1. write a method that asks user to enter a string.
+	        if the string is empty, print: string is empty
+	        if the string has more than 3 characters, print the last three characters
+	        if the string has less than or equal 3 characters, print the string itself
+ */
