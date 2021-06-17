1f0960d2a876e576eab97714b057c6b8123b0014
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-17 12:47:56 -0400

    day17

---
+++ src/day17_String/StringPractice.java
@@ -0,0 +1,50 @@
+package day17_String;
+
+import java.util.Scanner;
+
+public class StringPractice {
+
+    public static void main(String[] args) {
+        
+        String gmail = "Cybertek@yahoo.com";
+        /*
+        String domain = gmail.substring( gmail.indexOf("@"));
+        boolean isValidGmail = domain.equals("@gmail.com");
+         */
+
+        boolean isValidGmail = gmail.endsWith("@gmail.com");
+
+        System.out.println("isValidGmail = " + isValidGmail);
+
+
+        System.out.println("--------------------------------");
+
+        String url = "www.amazon.com";
+
+        boolean isValidUrl = url.startsWith("www.") &&
+                        (  url.endsWith(".com") || url.endsWith(".edu") || url.endsWith(".gov")  );
+
+        System.out.println("isValidUrl = " + isValidUrl);
+
+
+        System.out.println("-------------------------------");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a word:");
+        String word = scan.next();
+
+        boolean endsWithLY =  word.substring( word.length()-2).equals("ly");
+        boolean endsWithLY2 = word.endsWith("ly");
+
+        if(endsWithLY){
+            System.out.println("really?");
+        }else{
+            System.out.println("Never mind");
+        }
+
+
+        
+        
+        
+    }
+    
+}
