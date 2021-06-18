5fefad3715d31595af80c9c47f39bd1e5efaec96
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-18 10:57:45 -0400

    day18

--- src/day18_Recap/WithOutX.java
+++ src/day18_Recap/WithOutX.java
@@ -20,6 +20,28 @@ public class WithOutX {
 
         scan.close();
 
+        System.out.println("--------------------------------------");
+
+        String sentence = "I love Java programming language";
+
+        //java or Java ==> has Java
+        // otherwise ==> does not have Java
+
+        if(sentence.toLowerCase().contains("java")){ // we can ignore the case sensitivity by creating lowercase version of the sentence
+            System.out.println("Has Java");
+        }else{
+            System.out.println("Does not have Java");
+        }
+
+
+        System.out.println("-------------------------------------------------");
+
+        String s1 = "I love cats and dogs";
+
+      //  boolean lovesCats =  s1.equalsIgnoreCase("cats");  // verifies if s1 IS "cats"
+        boolean lovesCats = s1.toLowerCase().contains("cats"); // verifies if s1 HAS "cats
+
+
 
     }
 
