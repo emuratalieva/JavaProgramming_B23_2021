d14375b1d286226fd46f39efb44fbed2fa3be298
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-21 11:36:28 -0400

    day19

--- src/day19_Loops/ForLoopIntro.java
+++ src/day19_Loops/ForLoopIntro.java
@@ -1,4 +1,31 @@
 package day19_Loops;
 
 public class ForLoopIntro {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        System.out.println("---------------------------------------");
+
+        for(int i = 1;  i <= 10;  ){ //i : 1, 3, 5, 7
+            System.out.println("Hello World"); //3 times
+        }
+
+
+        System.out.println("Hello Cybertek");
+
+
+
+    }
+
 }
