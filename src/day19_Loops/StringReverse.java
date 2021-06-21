672034e9783ad425a7a12c83dde2133bc29d19d5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-21 15:43:01 -0400

    day19

--- src/day19_Loops/StringReverse.java
+++ src/day19_Loops/StringReverse.java
@@ -1,5 +1,23 @@
 package day19_Loops;
 
+import java.util.Scanner;
+
 public class StringReverse {
+
+    public static void main(String[] args) {
+        String name = new Scanner(System.in).nextLine();
+        //index:       01234567
+
+        String reverseName = "";  //rathuM
+
+        for (int i = name.length()-1; i >= 0; i--) {
+             reverseName += name.charAt(i); // getting the characters starting from last index to index 0
+        }
+
+        System.out.println(reverseName);
+
+
+
+    }
 
 }
