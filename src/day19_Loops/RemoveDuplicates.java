b131a3fb112ee70215d91dfb8721541035a4ac51
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-21 10:29:48 -0400

    day19

--- src/day19_Loops/RemoveDuplicates.java
+++ src/day19_Loops/RemoveDuplicates.java
@@ -1,2 +1,51 @@
-package day19_Loops;public class RemoveDuplicates {
+package day19_Loops;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        String word = "aabbc";
+
+        String result = ""; //"abc"
+
+        if(word.length() == 5 ) {
+            if (!result.contains("" + word.charAt(0))) { //'a'
+                result += word.charAt(0);
+            }
+            if (!result.contains("" + word.charAt(1))) { //'a'
+                result += word.charAt(1);
+            }
+            if (!result.contains("" + word.charAt(2))) { //'b'
+                result += word.charAt(2);
+            }
+            if (!result.contains("" + word.charAt(3))) { //'b'
+                result += word.charAt(3);
+            }
+            if (!result.contains("" + word.charAt(4))) { //'c'
+                result += word.charAt(4);
+            }
+
+        }else if(word.length() > 5){
+            result = "Too long";
+        }else{ //word.length() < 5
+            result = "Too Short";
+        }
+
+
+
+    }
+
 }
+
+
+/*
+1. ask the user to enter a word. the word must be five characters long and print the word without the duplicated characters, but if the word is less than five characters long, print "Too Short." and if the word is more than five characters long, print "Too long."
+
+		Ex:
+			input:
+				aabbc
+
+			 output:
+			 	abc
+
+ */
