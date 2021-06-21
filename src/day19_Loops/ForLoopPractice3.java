39bdac1238a9d977b117745dea0f1e1cab7cae59
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-21 12:42:59 -0400

    day19

--- src/day19_Loops/ForLoopPractice3.java
+++ src/day19_Loops/ForLoopPractice3.java
@@ -4,9 +4,6 @@ public class ForLoopPractice3 {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 50 ; i++) {
-            System.out.println("Push Up "+i);
-        }
 
 
 
