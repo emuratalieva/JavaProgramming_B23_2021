48c6da39df990171d06ee9230919aecad6d0e4f3
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-21 15:51:38 -0400

    day19

---
+++ src/day19_Loops/Palindrome.java
@@ -0,0 +1,26 @@
+package day19_Loops;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+    //    racecar
+        System.out.println("Enter a word:");
+        String word = new Scanner(System.in).next();
+        String reverseWord = "";  //Level ==> leveL
+
+        for (int i = word.length()-1; i >= 0 ; i--) {
+           reverseWord += word.charAt(i);
+        }
+
+        boolean isPalindrome = word.equalsIgnoreCase(reverseWord);
+
+        System.out.println("isPalindrome = " + isPalindrome);
+
+
+    }
+
+
+}
