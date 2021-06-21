d4e16801f481390543111fa03fbe645bdd305f7e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-21 13:00:43 -0400

    day19

--- src/day19_Loops/ForLoopPractice3.java
+++ src/day19_Loops/ForLoopPractice3.java
@@ -4,9 +4,62 @@ public class ForLoopPractice3 {
 
     public static void main(String[] args) {
 
+        for (int i = 1; i <= 8 ; i++) { // i:0,1,2,3,4,5,6,7,8,    9
+            System.out.println("\t\t* * * * * * ");
+
+        }
+
+
+        System.out.println("--------------------------------------------");
+
+        System.out.println("* * * * * *");
+        for (int i = 1; i <=8 ; i++) {
+            System.out.println("*         *");
+        }
+        System.out.println("* * * * * *");
+
+        System.out.println("--------------------------------------------");
+
+        int sum = 0;
+
+        for (int i = 1; i <= 100 ; i++) {
+            sum += i;
+        }
+
+        System.out.println("sum = " + sum);
 
 
 
     }
 
 }
+/*
+2.  print the following shape:
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+                * * * * * *
+
+
+3. print the following shape:
+                        * * * * * *
+                        *         *
+                        *         *
+                        *         *
+                        *         *
+                        *         *
+                        *         *
+                        *         *
+                        *         *
+                        * * * * * *
+
+    Hint: in the loop body , only place the statements that you need to repeat
+
+
+4. write a program that can calculate the sum of all numbers between 1 to 100
+
+ */
