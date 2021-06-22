167fc08a4195e58ac643a640044963efcaebe689
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-22 16:10:02 -0400

    day20

--- src/day20_Loops/BranchingStatements.java
+++ src/day20_Loops/BranchingStatements.java
@@ -25,6 +25,43 @@ public class BranchingStatements {
 
         }
 
+        System.out.println("--------------------------------------");
+        // print the odd numbers between 1 ~ 10.  MUST use continue
+
+        for (int i = 1; i < 11 ; i++) {
+
+            if(i%2==0){ // if the i is even number
+                continue; // skip
+            }
+
+            System.out.println(i);
+
+        }
+
+
+        System.out.println("-------------------------------------------");
+        // print all the numbers between 1- 100 that are evenly divisible by 3 & 5
+
+        for(int i = 1; i <= 100; i++){
+            if(i % 15 !=0){
+                continue;
+            }
+            System.out.print(i+" ");
+
+        }
+
+        System.out.println();
+
+        System.out.println("-----------------------------------------------");
+        // print all letters between A ~ Z, except for D, T, X
+
+        for (char i ='A'; i <= 'Z'; i++){
+            if(i == 'D' || i == 'T' || i == 'X'){
+                continue;
+            }
+
+            System.out.print(i+" ");
+        }
 
 
 
