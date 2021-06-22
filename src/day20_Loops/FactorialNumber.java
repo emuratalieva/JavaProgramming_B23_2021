678cc225add2b32c855476f355f24087fc9ceb23
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-22 14:25:51 -0400

    day20

---
+++ src/day20_Loops/FactorialNumber.java
@@ -0,0 +1,29 @@
+package day20_Loops;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter a number:");
+        int number = new Scanner(System.in).nextInt();  // 5 * 4 * 3 * 2 * 1
+        int result = 1; //
+
+        for(int i =  number;  i >= 1;  i-- ){ //i: 5, 4, 3, 2, 1
+            result *= i;
+        }
+
+        System.out.println("result = " + result);
+
+
+    }
+
+}
+/*
+1. Write a program that can return the factorial number of any given number
+
+            Ex:
+                input: 5
+                output: 120  ( because: 5! = 5 * 4 * 3 * 2* 1 = 120 )
+ */
