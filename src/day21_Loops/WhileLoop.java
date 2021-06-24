217dad2bc5d3e78ce30f7e0760ff6a6498f1bc61
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-24 12:04:20 -0400

    day21

---
+++ src/day21_Loops/WhileLoop.java
@@ -0,0 +1,25 @@
+package day21_Loops;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        if(true){
+            System.out.println("Hello batch 23");
+        }
+
+        System.out.println("------------------------------");
+       // while loop: repeated if statement
+
+        while(true){
+            System.out.println("Hello batch 23");
+            
+        }
+
+
+
+
+
+    }
+
+}
