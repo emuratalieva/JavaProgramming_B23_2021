f14cdd825d75960cd0b1ead081279f5658fa5cba
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-24 10:17:30 -0400

    day21

---
+++ src/day21_Loops/MultiplyTwoNumbers.java
@@ -0,0 +1,25 @@
+package day21_Loops;
+
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+        int a = 10,
+            b= -5;
+
+        
+        int result = 0;
+        
+        for(int i = 0; i < b; i++){
+            result += a;
+        }
+
+        System.out.println("result = " + result);
+        
+        
+        
+    }
+}
+
+/*
+1. write a program that can multiply two positive numbers without using * operator
+ */
