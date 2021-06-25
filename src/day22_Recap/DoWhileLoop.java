a4257a5595e3d46a7dc253c8545efcdea5883791
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-25 11:09:55 -0400

    day22

---
+++ src/day22_Recap/DoWhileLoop.java
@@ -0,0 +1,24 @@
+package day22_Recap;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+        int i = 10;
+
+        while(i < 5){
+            System.out.println("Hello Cybertek");
+        }
+
+        System.out.println("--------------------------------");
+
+        do{
+            System.out.println("Hello Cybertek");
+        }while(i<5); 
+        // even if the condition is false, do-while loop gets executed one time
+
+
+
+    }
+
+
+}
