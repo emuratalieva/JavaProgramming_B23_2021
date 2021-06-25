89ce8f671cb8babc69c7c19a61a8cd37eb9b821f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-25 16:59:13 -0400

    day22

--- src/day22_Recap/RoomReservation.java
+++ src/day22_Recap/RoomReservation.java
@@ -12,6 +12,12 @@ public class RoomReservation {
         System.out.println("Would you like to reserve a room?");
         String answer = scan.nextLine().toLowerCase();
 
+        while(  ! ( answer.equals("yes") || answer.equals("no") )   ){ //to make sure user enters either yes or no
+            System.out.println("Would you like to reserve a room?");
+            answer = scan.nextLine().toLowerCase();
+        }
+
+
        while(answer.equals("yes")){
             System.out.println("Which bed type do you want?"); // king bed, queen bed, single bed
             String bedType = scan.nextLine().toLowerCase();
@@ -28,10 +34,17 @@ public class RoomReservation {
 
             System.out.println("Do you want to reserve another room?");
             answer = scan.nextLine().toLowerCase();
+
+           while(  ! ( answer.equals("yes") || answer.equals("no") )   ){ //to make sure user enters either yes or no
+               System.out.println("Invalid Entry, please re-enter! Do you want to reserve another room?");
+               answer = scan.nextLine().toLowerCase();
+           }
+
+
         }
 
 
-        System.out.println("Your total price is: "+totalPrice);
+        System.out.println("Your total price is: $"+totalPrice);
 
 
 
