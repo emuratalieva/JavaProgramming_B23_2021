2b11d04aa1fc01df092e071dbac1105167b65bb8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-28 14:45:11 -0400

    day23

--- src/day23_Arrays/HighestAndLowestScores.java
+++ src/day23_Arrays/HighestAndLowestScores.java
@@ -5,6 +5,8 @@ public class HighestAndLowestScores {
     public static void main(String[] args) {
 
         int[] scores = {85, 70, 95, 90, 100};
+        //              0,  1,  2,  3,  4
+
         String[] names = {"Mike", "Adam", "Tonny", "John", "Ammy"};
 
 
