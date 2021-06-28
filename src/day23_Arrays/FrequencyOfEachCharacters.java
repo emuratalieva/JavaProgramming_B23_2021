5e0ed2ac8073bc95f248613bf0381656b50a8940
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-28 11:42:58 -0400

    day23

--- src/day23_Arrays/FrequencyOfEachCharacters.java
+++ src/day23_Arrays/FrequencyOfEachCharacters.java
@@ -6,8 +6,8 @@ public class FrequencyOfEachCharacters {
 
         String str = "aabccc";
 
+        // step 1
         String removeDup = ""; //abc
-
         for (int i = 0; i <= str.length()-1 ; i++) {
             char ch = str.charAt(i);
 
@@ -18,15 +18,29 @@ public class FrequencyOfEachCharacters {
             removeDup += ch;
         }
 
-
-        System.out.println(removeDup);
-
-
-
         String result = "";  //a2b1c3
                              //a b c
                              //2 1 3
 
+        // step3
+        for(int j = 0; j <= removeDup.length()-1; j++){ // 0 1 2, outer loop is used for getting each characters from string removeDup
+
+            //step2
+            char ch = removeDup.charAt(j);  //'a', 'b', 'c'
+            int frequency = 0; // for the frequencies of ch: 2  1  3
+            for (int i = 0; i <= str.length()-1 ; i++) {  // inner loop is responsible for returning the frequency of ch
+                char each = str.charAt(i);
+                if(each == ch){
+                    frequency++;
+                }
+            }
+
+            result +=  ""+ch + frequency;
+
+        }
+
+
+        System.out.println(result);
 
 
 
@@ -42,4 +56,9 @@ public class FrequencyOfEachCharacters {
 
 			output:
 				a2b1c3d1
+
+	step1: need to know how to remove duplicates
+	step2: need to know how to fidn the frequency of one character
+	step3: use the loop and apply step 2 to the remaining characters of the string
+
  */
