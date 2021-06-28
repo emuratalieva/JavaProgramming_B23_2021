4278c6ef1348ee5a1c4f9170853bc1a80bd1c852
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-28 15:04:30 -0400

    day23

--- src/day23_Arrays/HighestAndLowestScores.java
+++ src/day23_Arrays/HighestAndLowestScores.java
@@ -4,10 +4,10 @@ public class HighestAndLowestScores {
 
     public static void main(String[] args) {
 
-        int[] scores = {85, 70, 95, 90, 100};
+        int[] scores = {85, 70, 95, 90, 100, 110, 55};
         //              0,  1,  2,  3,  4
 
-        String[] names = {"Mike", "Adam", "Tonny", "John", "Ammy"};
+        String[] names = {"Mike", "Adam", "Tonny", "John", "Ammy", "Ahmet", "Muhtar"};
 
         int maxScore = scores[0];
         String maxName= "";
@@ -16,26 +16,22 @@ public class HighestAndLowestScores {
         String minName = "";
 
         for (int i=0; i <= names.length-1; i++ ){
-            String eachName = names[i];
-            int eachScore = scores[i];
+            String eachName = names[i]; // gets each names from the array names
+            int eachScore = scores[i]; // gets each score from the array scores
 
-            if(eachScore > maxScore ){
+            if(eachScore > maxScore ){ // used for comparing the scores to assign the maximum score to maxScore
                 maxScore = eachScore;
                 maxName = eachName;
             }
 
-            if(eachScore < minScore){
+            if(eachScore < minScore){ // used for comparing the scores to assign the miximum score to minScore
                 minScore = eachScore;
                 minName = eachName;
             }
-
         }
 
-        System.out.println("maxScore = " + maxScore);
-        System.out.println("maxName = " + maxName);
-
-        System.out.println("minScore = " + minScore);
-        System.out.println("minName = " + minName);
+        System.out.println(maxName +" : "+maxScore);
+        System.out.println(minName+" : "+minScore);
 
 
     }
