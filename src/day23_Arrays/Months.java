12f1b2d41e860bd03cecb862055e5de9518ec256
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-28 12:39:20 -0400

    day23

---
+++ src/day23_Arrays/Months.java
@@ -0,0 +1,27 @@
+package day23_Arrays;
+
+import java.util.Scanner;
+
+public class Months {
+
+    public static void main(String[] args) {
+        //                    1                                                                         12
+        String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        // index:           0       1      2      3      4      5      6      7       8     9     10      11
+
+        System.out.println("Enter a number");
+        int n = new Scanner(System.in).nextInt();
+
+        if(n>=1 && n<=12) {
+            System.out.println(months[n - 1]); // index: 12
+        }else{
+            System.out.println("Invalid Number");
+        }
+
+
+
+
+    }
+
+
+}
