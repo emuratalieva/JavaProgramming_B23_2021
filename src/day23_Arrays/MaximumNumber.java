0769d9bae0d48e38a062a413128d8195c46a9f23
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-28 13:10:44 -0400

    day23

---
+++ src/day23_Arrays/MaximumNumber.java
@@ -0,0 +1,25 @@
+package day23_Arrays;
+
+public class MaximumNumber {
+
+
+
+}
+/*
+ write a program that can find the maximum number from an array of integers
+
+    ex:
+            array = {10,5, 4, 400, 50, 0, -1};
+
+            output: 400
+
+
+write a program that can find the minimum number from an array of integers
+
+    ex:
+            array = {10,5, 4, 400, 50, 0, -1};
+
+            output: -1
+
+
+ */
