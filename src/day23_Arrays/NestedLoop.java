72cf16d404b190b751e00f2ebbbe7ec5f4e7dcf7
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-28 10:36:56 -0400

    day23

---
+++ src/day23_Arrays/NestedLoop.java
@@ -0,0 +1,50 @@
+package day23_Arrays;
+
+public class NestedLoop {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5 ; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 5 ; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 5 ; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 5 ; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 5 ; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+
+        System.out.println("---------------------------------------");
+
+        for(int j = 6; j <= 10; j++){ // outer loop
+            for (int i = 1; i <= 5 ; i++) { // inner loop
+                System.out.print(i+" ");
+            }
+            System.out.println();
+
+        }
+
+
+
+
+
+    }
+
+
+}
