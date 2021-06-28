3ea3d3c35cfb8b36a1e247fb672d0c86369bac0b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-28 14:37:45 -0400

    day23

--- src/day23_Arrays/MaximumNumber.java
+++ src/day23_Arrays/MaximumNumber.java
@@ -3,6 +3,37 @@ package day23_Arrays;
 public class MaximumNumber {
 
 
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 5, 4, 400, 50, 0, -1};
+        int max = numbers[0]; // 400
+        int min = numbers[0];
+
+        for (int i = 0; i <= numbers.length -1 ; i++) {
+
+            int each = numbers[i]; // each: 10, 5, 4, 400, 50, 0, -1
+
+            if(each > max){ // if any greater number is occurred
+                max = each;
+            }
+
+            if(each < min){
+                min = each;
+            }
+
+        }
+
+
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+
+
+
+
+
+    }
+
+
 
 }
 /*
