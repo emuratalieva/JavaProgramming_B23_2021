dabe7d267e8c45f491d271b171c8b8ea29530ca3
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-28 12:08:48 -0400

    day23

---
+++ src/day23_Arrays/ArraysIntro.java
@@ -0,0 +1,27 @@
+package day23_Arrays;
+
+public class ArraysIntro {
+
+    //DataType[] variableName = {data1, data2, data3 ...};
+    public static void main(String[] args) {
+
+        String[]  group1 = {"Feruza" , "Yuliang" , "Tamara", "Ahmet" };
+        //          index:   0           1           2         3
+
+        // retrieve data from array:
+        System.out.println( group1[0] );
+        System.out.println( group1[1] ); // Yuliang
+        System.out.println( group1[2] ); // Tamara
+        System.out.println( group1[3] );
+
+        System.out.println("---------------------------------------------");
+
+        for(int i=0; i <= 3; i++){
+            System.out.println( group1[i] );
+        }
+
+
+
+    }
+
+}
