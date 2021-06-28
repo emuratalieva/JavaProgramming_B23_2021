106023cb410cb077593a08a9f21667a5a80bb63e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-28 14:57:29 -0400

    day23

---
+++ src/day23_Arrays/AverageNumbers.java
@@ -0,0 +1,12 @@
+package day23_Arrays;
+
+public class AverageNumbers {
+
+
+}
+/*
+    4. AverageNumber
+    		1. Ask the user how many numbers he/she wanst to enter
+    		2. get all the inputs from the user
+    		3. return the average number
+ */
--- src/day23_Arrays/HighestAndLowestScores.java
+++ src/day23_Arrays/HighestAndLowestScores.java
@@ -9,6 +9,34 @@ public class HighestAndLowestScores {
 
         String[] names = {"Mike", "Adam", "Tonny", "John", "Ammy"};
 
+        int maxScore = scores[0];
+        String maxName= "";
+
+        int minScore = scores[0];
+        String minName = "";
+
+        for (int i=0; i <= names.length-1; i++ ){
+            String eachName = names[i];
+            int eachScore = scores[i];
+
+            if(eachScore > maxScore ){
+                maxScore = eachScore;
+                maxName = eachName;
+            }
+
+            if(eachScore < minScore){
+                minScore = eachScore;
+                minName = eachName;
+            }
+
+        }
+
+        System.out.println("maxScore = " + maxScore);
+        System.out.println("maxName = " + maxName);
+
+        System.out.println("minScore = " + minScore);
+        System.out.println("minName = " + minName);
+
 
     }
 
