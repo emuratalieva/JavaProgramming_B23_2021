cd2b62407da2599327e329c84b80e0c9d275f4b3
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-28 14:40:51 -0400

    day23

---
+++ src/day23_Arrays/HighestAndLowestScores.java
@@ -0,0 +1,14 @@
+package day23_Arrays;
+
+public class HighestAndLowestScores {
+
+    public static void main(String[] args) {
+
+        int[] scores = {85, 70, 95, 90, 100};
+        String[] names = {"Mike", "Adam", "Tonny", "John", "Ammy"};
+
+
+    }
+
+
+}
