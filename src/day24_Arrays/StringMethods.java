d6f79a2edd71a29e49774d9f273bb7903a2815e5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-29 12:15:39 -0400

    day24

---
+++ src/day24_Arrays/StringMethods.java
@@ -0,0 +1,36 @@
+package day24_Arrays;
+
+import java.util.Arrays;
+
+public class StringMethods {
+
+    public static void main(String[] args) {
+        // split
+        String sentence = "I love learning java programming language";
+
+        String[] words =  sentence.split(" ");
+
+        System.out.println(Arrays.toString(words));
+
+        for (int i = words.length-1; i >= 0; i--) {
+            System.out.print(words[i]+" ");
+        }
+
+        System.out.println();
+
+        String email = "James@gmail.com";
+
+         String[] arrays =  email.split("@"); //{James, gmail.com}
+        System.out.println(Arrays.toString(arrays));
+
+        String firstname = arrays[0];
+
+       System.out.println("firstName = " + firstname);
+
+
+
+
+
+    }
+
+}
