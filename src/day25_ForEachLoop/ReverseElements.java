2a01f13e611211a73de7191fdbe4a3d0d5d4c1f2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-01 10:28:32 -0400

    day25

---
+++ src/day25_ForEachLoop/ReverseElements.java
@@ -0,0 +1,42 @@
+package day25_ForEachLoop;
+
+import java.util.Arrays;
+
+public class ReverseElements {
+
+    public static void main(String[] args) {
+        String[] array = {"Java", "Python", "C#"};
+
+        for (int j = 0; j < array.length; j++) { // j: index numbers of the array ( starting from 0)
+
+            String element = array[j]; //element of the array
+            //0123
+            String reverse = ""; //to store the reversed version of the element
+
+            for (int i = element.length()-1; i >=0 ; i--) { // index numbers of the String
+                reverse += element.charAt(i); // gets the characters from the string element
+            }
+
+            array[j] = reverse; // assigning reversed element to the index of the array
+
+        }
+
+
+
+
+        System.out.println(Arrays.toString(array));
+
+
+
+    }
+
+}
+
+/*
+1. write a program that can  reverse each elements in an array of string
+		Ex:
+		    array = {"Java", "Python", "C#"}
+
+	    	output:
+	    		["avaJ", "nohtyP", "#C"]
+ */
