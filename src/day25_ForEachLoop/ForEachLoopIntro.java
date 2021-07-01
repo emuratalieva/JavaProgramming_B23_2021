7d08fde93011cbd63c982afb8148a2c023f5c52b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-01 11:22:46 -0400

    day25

---
+++ src/day25_ForEachLoop/ForEachLoopIntro.java
@@ -0,0 +1,19 @@
+package day25_ForEachLoop;
+
+public class ForEachLoopIntro {
+
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1,2,3,4,5};
+        //index:         0 1 2 3 4
+
+        for (int i = 0; i < numbers.length; i--) {
+            System.out.println(numbers[i]);
+        }
+
+
+
+    }
+
+}
