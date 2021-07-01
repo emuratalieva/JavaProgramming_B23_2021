886db8d06360348af4b9cb7420f138b9f77d5de3
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-01 10:55:57 -0400

    day25

---
+++ src/day25_ForEachLoop/MergeTwoArrays.java
@@ -0,0 +1,44 @@
+package day25_ForEachLoop;
+
+import java.util.Arrays;
+
+public class MergeTwoArrays {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1,2,3,4},
+               arr2 = {5,6};
+
+        int[] arr3 = new int[arr1.length + arr2.length]; // making sure that arr3 has enough capacity to contain the elements of arr1 and arr2
+        //{1,2,3,4,0,0}
+
+
+       int j = 0; // for the index numbers of third array
+        for (int i = 0; i < arr1.length ; i++) { // for arr1
+            arr3[j++] = arr1[i]; // retrieves each element from arr1 and assigns to the indexes of arr3
+        }
+
+
+        for (int i = 0; i < arr2.length; i++) { // for arr2
+            arr3[j++] = arr2[i]; // retrieves each element from arr2 and assigns to the indexes of arr3
+        }
+
+
+        System.out.println(Arrays.toString(arr3)  );
+
+
+
+
+    }
+
+}
+
+/*
+2. write a program that can merge two arrays of integers
+		Ex:
+			arr1 = {1,2,3,4}
+			arr2 = {5,6}
+
+		output
+			arr3 = {1,2,3,4,5,6}
+ */
