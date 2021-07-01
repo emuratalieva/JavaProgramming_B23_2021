b1323389a200f767eef9873f8a297eb570863cfc
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-01 10:36:43 -0400

    day25

--- src/day25_ForEachLoop/ReverseElements.java
+++ src/day25_ForEachLoop/ReverseElements.java
@@ -5,24 +5,22 @@ import java.util.Arrays;
 public class ReverseElements {
 
     public static void main(String[] args) {
-        String[] array = {"Java", "Python", "C#"};
+        String[] array = {"Java", "Python", "C#", "Cybertek", "School", "Selenium", "API"};
 
-        for (int j = 0; j < array.length; j++) { // j: index numbers of the array ( starting from 0)
+        for (int j = 0; j < array.length; j++) { // j: index numbers of the array ( starting from index 0 to last index)
 
-            String element = array[j]; //element of the array
+            String element = array[j]; //each element of the array
             //0123
             String reverse = ""; //to store the reversed version of the element
 
             for (int i = element.length()-1; i >=0 ; i--) { // index numbers of the String
                 reverse += element.charAt(i); // gets the characters from the string element
+                // this inner loop is responsible for reversing the string
             }
 
             array[j] = reverse; // assigning reversed element to the index of the array
 
         }
-
-
-
 
         System.out.println(Arrays.toString(array));
 
@@ -39,4 +37,14 @@ public class ReverseElements {
 
 	    	output:
 	    		["avaJ", "nohtyP", "#C"]
+
+2. write a program that can count how many palindromes in an array of string
+        Ex:
+            {"anna", "level", "Java"};
+
+            output:
+                2
+
+        DO NOT CHANGE THE ELEMENTS
+
  */
