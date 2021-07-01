4b09395b5b47702d2d3b1864a77dee39145761ce
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-01 11:54:50 -0400

    day25

--- src/day25_ForEachLoop/ForEachLoopIntro.java
+++ src/day25_ForEachLoop/ForEachLoopIntro.java
@@ -8,9 +8,30 @@ public class ForEachLoopIntro {
         int[] numbers = {1,2,3,4,5};
         //index:         0 1 2 3 4
 
-        for (int i = 0; i < numbers.length; i--) {
+        for (int i = 0; i < numbers.length; i++) { // i: index numbers of the array
             System.out.println(numbers[i]);
         }
+
+        System.out.println("-------------------------------");
+
+        for(int  each  : numbers ){ // each: elements of the array
+            System.out.println(each);
+        }
+
+
+        System.out.println("-------------------------------------");
+
+        String[] names = {"Ahmet", "Boburbek", "Farid", "Robinson"};
+
+        for(String eachName  : names){  // there is no index in for each loop
+            System.out.println(eachName);
+        }
+
+        // we can use for each loop if we don't need to use indexes
+        // we cannot alter the index numbers
+
+
+
 
 
 
