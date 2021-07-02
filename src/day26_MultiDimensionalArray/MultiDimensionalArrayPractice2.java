39b1b52f782f2fd2220d6fc1f7fe67201d9bbdb4
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-02 16:21:42 -0400

    day26

---
+++ src/day26_MultiDimensionalArray/MultiDimensionalArrayPractice2.java
@@ -0,0 +1,24 @@
+package day26_MultiDimensionalArray;
+
+public class MultiDimensionalArrayPractice2 {
+
+    public static void main(String[] args) {
+        /*
+        String[] group5 = {"Ahmed", "Ahmet", "Igor", "Andrei", "Mykyta"},
+                group2 = {"Sabir", "Abbos", "Kseniia", "Vitalii", "Merve", "Atila", "Shokhzod", "Hende", "Serife", "Collins", "Akangankoy", "Liubov"},
+                group3 = {"Oz", "Ibrahim", "Davut", "Dontrell", "Muahmmed"},
+                group4 = {"Inesa","Aysu","Suat","Esra","Yusuf","Mohamed","Tinbite","Raphael"},
+                group1 = {"Selda", "Ibrahim", "Tamerlan",  "Mehmut", "Sina"};
+*/
+
+        String[][] cybertekGroups = {
+
+        };
+
+
+
+
+    }
+
+
+    }
