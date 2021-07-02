3d6e1ab328f04c50cfe01f4143a7456545f3864a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-02 16:38:58 -0400

    day26

--- src/day26_MultiDimensionalArray/MultiDimensionalArrayPractice2.java
+++ src/day26_MultiDimensionalArray/MultiDimensionalArrayPractice2.java
@@ -12,8 +12,48 @@ public class MultiDimensionalArrayPractice2 {
 */
 
         String[][] cybertekGroups = {
-
+                {"Ahmed", "Ahmet", "Igor", "Andrei", "Mykyta"},  //0
+                {"Sabir", "Abbos", "Kseniia", "Vitalii", "Merve", "Atila", "Shokhzod", "Hende", "Serife", "Collins", "Akangankoy", "Liubov"},
+                {"Oz", "Ibrahim", "Ahmed", "Davut", "Dontrell", "Muahmmed"},
+                {"Inesa","Aysu","Suat","Esra", "Ahmed","Yusuf","Mohamed","Tinbite","Raphael"},
+                {"Selda", "Ibrahim", "Tamerlan",  "Mehmut", "Sina"}
         };
+
+        int count = 0;
+
+        for (String[] eachGroup : cybertekGroups) {
+            for (String eachName : eachGroup) {
+                if(eachName.equals("Ahmed") ){
+                    count++;
+                }
+
+            }
+        }
+
+        System.out.println("count = " + count);
+
+
+        System.out.println("------------------------------------------");
+        String[][][][][] names = { {  {{{"Viktoria", "Feruza"},{"Koray", "Break"} }, {{"Komiljon", "Yasin"}}}   , { {{"Alexandra", "Merve"}, {"Zaur", "Vulkasin"} }}} };
+
+
+        for (String[][][][] each4D : names) {
+            for (String[][][] each3D : each4D) {
+                for (String[][] each2D : each3D) {
+                    for (String[] each1D : each2D) {
+                        for (String element : each1D) {
+
+                            System.out.println(element);
+
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+
 
 
 
