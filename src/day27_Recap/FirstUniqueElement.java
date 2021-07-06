9b731c8f6734e152c16393718b9b70f7d02967c0
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-06 10:54:34 -0400

    day27

--- src/day27_Recap/FirstUniqueElement.java
+++ src/day27_Recap/FirstUniqueElement.java
@@ -2,7 +2,53 @@ package day27_Recap;
 
 public class FirstUniqueElement {
 
+
+
     public static void main(String[] args) {
+        int[] nums = {1,1,2,3,3,4,1,1,1,1};
+        int firstUniqueElement = 0;
+
+/*
+        for(int j =0; j < nums.length; j++) {// outer loop: responsible for repeating the inner loop to find frequency of each element
+
+            int frequency = 0;
+            for (int i = 0; i < nums.length; i++) {  // inner loop: responsible for finding the frequency of one element
+                if (nums[i] == nums[j]) {
+                    frequency++;
+                }
+            }
+
+            if(frequency == 1){ // if element is unique
+               firstUniqueElement = nums[j];
+               break;  // to make sure only the first unique element is returned from the loop
+            }
+
+        }
+
+ */
+
+        for(int each  : nums ) {// outer loop: responsible for repeating the inner loop to find frequency of each element
+
+            int frequency = 0;
+            for (int element : nums) {  // inner loop: responsible for finding the frequency of one element
+                if (element == each) {
+                    frequency++;
+                }
+            }
+
+            if(frequency == 1){ // if element is unique
+                firstUniqueElement = each;
+                break;  // to make sure only the first unique element is returned from the loop
+            }
+
+        }
+
+
+       System.out.println("firstUniqueElement = " + firstUniqueElement);
+
+
+
+
 
     }
 
