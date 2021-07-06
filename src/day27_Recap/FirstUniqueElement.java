cbff085ae5c94bac5b6fe278d1bf0ebdd1cec48b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-06 10:30:32 -0400

    day27

---
+++ src/day27_Recap/FirstUniqueElement.java
@@ -0,0 +1,13 @@
+package day27_Recap;
+
+public class FirstUniqueElement {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+/*
+write a program that can return the first unique element from the arrat
+ */
