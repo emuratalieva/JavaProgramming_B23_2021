bfa0643bb6a42c3fd51b247f1211d7ce493315ee
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-08 11:28:59 -0400

    day28

--- src/day28_Methods/MethodsWithParameter.java
+++ src/day28_Methods/MethodsWithParameter.java
@@ -3,7 +3,26 @@ package day28_Methods;
 public class MethodsWithParameter {
 
 
-    public static 
+    public static void main(String[] args) {
+
+        eligible(30);
+
+    }
+
+
+    public static void eligible(int age){
+
+        if(age >= 21){
+            System.out.println("Eligible");
+        }else{
+            System.out.println("Not Eligible");
+        }
+
+    }
+
+
+
+
 
 
 }
