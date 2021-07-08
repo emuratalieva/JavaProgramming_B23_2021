c6f7efeebea28c37cb5d3b339c7efa9b2300cdb5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-08 10:15:36 -0400

    day28

---
+++ src/day28_Methods/PracticeWithoutMethod.java
@@ -0,0 +1,41 @@
+package day28_Methods;
+
+public class PracticeWithoutMethod {
+
+    public static void main(String[] args) {
+
+        // step1
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+        }
+
+        //step 2
+        System.out.println("Muhtar");
+
+        //step 3
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+        }
+
+        // step4:
+        System.out.println("Cybertek");
+
+        //step 5:
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+        }
+
+
+    }
+
+}
+
+
+/*
+step1: print hello 5 times (loop)
+step2: print your name
+step3: print hello 5 times (loop)
+step4: print your school name
+step5: print hello 5 times (loop)
+
+ */
