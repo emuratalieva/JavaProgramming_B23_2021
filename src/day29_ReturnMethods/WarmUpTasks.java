3e79ac864a3296b20582192b7aee1f272a024952
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-09 11:00:37 -0400

    day29

--- src/day29_ReturnMethods/WarmUpTasks.java
+++ src/day29_ReturnMethods/WarmUpTasks.java
@@ -1,11 +1,36 @@
 package day29_ReturnMethods;
 
+import java.util.Arrays;
+
 public class WarmUpTasks {
 
     public static void main(String[] args) {
 
         posNegZero(-10);
 
+        System.out.println("-----------------------------------");
+
+        int[] numbers = {10, 20, 0, -30, -5};
+
+        for (int number : numbers) { // 10, 20, 0, -30, -5
+            posNegZero(number);
+        }
+
+        System.out.println("---------------------------------------");
+        int[] array1 = {1,2,3,4},
+                array2 = {5,6,7,8,9,10};
+
+        mergeTwoArrays(array1, array2);
+
+        int[] arr1 ={100,200,300,400},
+                arr2 = {1000, 2000, 4000};
+
+        mergeTwoArrays(arr1, arr2);
+
+
+        System.out.println("------------------------------------------");
+
+        formatFullName("cyBERtek", "scHooL");
 
 
     }
@@ -23,7 +48,32 @@ public class WarmUpTasks {
 
     }
 
+    public static void mergeTwoArrays(int[] arr1, int[] arr2){
 
+        int[] arr3 = new int[arr1.length+arr2.length];
+        int i = 0;
+        for (int each : arr1) {
+           arr3[i++] = each;
+        }
+
+        for (int each : arr2) {
+            arr3[i++] = each;
+        }
+
+        System.out.println(Arrays.toString(arr3));
+
+    }
+
+    public static void formatFullName(String firstName, String lastname){
+
+       firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase();
+       lastname = lastname.substring(0, 1).toUpperCase() + lastname.substring(1).toLowerCase();
+
+       String fullName = firstName +" "+lastname;
+
+        System.out.println("fullName = " + fullName);
+
+    }
 
 
 }
