7a2b9722356b0b0dd733ff21c3ba0831f32f65f4
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-12 12:49:21 -0400

    day30

--- src/day30_Recap/LoopsReview3.java
+++ src/day30_Recap/LoopsReview3.java
@@ -20,6 +20,50 @@ public class LoopsReview3 {
         System.out.println( sentence);
 
 
+        System.out.println("----------------------------------------");
+
+        for(int j = 1; j<= 5; j++){
+            for(int i=1; i <= 10; i++){
+                System.out.print(i+ " ");
+            }
+            System.out.println();
+
+        }
+
+        System.out.println("----------------------------------------");
+
+        String str = "aabcccd";
+
+
+
+        for(int j = 0; j <= str.length()-1; j++){
+
+            char ch = str.charAt(j);
+            int frequency = 0;
+
+
+            for(int i= 0; i<= str.length()-1 ; i++ ){ // find the frequency of one character
+                if(str.charAt(i) == ch){  //
+                    frequency++;
+                }
+            }
+
+          //  System.out.println(ch +"-"+frequency);
+
+            if(frequency ==1){
+                System.out.print(ch);
+            }
+
+
+        }
+
+
+
+
+
+
+
+
 
 
 
