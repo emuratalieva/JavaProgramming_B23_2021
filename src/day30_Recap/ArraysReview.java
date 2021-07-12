499ce911b77a864a2c43e62cee9b3d8a39b9eb08
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-12 15:05:05 -0400

    day30

--- src/day30_Recap/ArraysReview.java
+++ src/day30_Recap/ArraysReview.java
@@ -36,6 +36,44 @@ public class ArraysReview {
         }
 
 
+        System.out.println("-----------------------------------------------------");
+
+        int[] nums = {10, 20, 30, 40, 50};
+        // index:     0,  1,  2,  3,   4
+
+        int[] result = new int[nums.length]; //[50, 40, 30, 20, 10]
+        //                                      0,  1,   2,  3,  4
+
+        for(int i = nums.length-1, j = 0 ; i >=0; i-- , j++){
+           result[j]  = nums[i];
+        }
+
+        System.out.println(Arrays.toString(result));
+
+
+        System.out.println("--------------------------------------------");
+
+        String[] group1 = {"Sophie", "Feruza", "Ekaterina"}; // i
+        String[] group2 = {"Shazia", "Bilguun", "Danka", "Mucahit"}; //j
+
+        String[] group3 = new String[group1.length + group2.length]; //k
+
+        int k = 0;
+        for(int i = 0; i <= group1.length-1; i++ , k++){
+            group3[k] = group1[i]; //
+        }
+
+        for(int j = 0; j <= group2.length-1; j++, k++){
+            group3[k] = group2[j];
+        }
+
+
+        System.out.println( Arrays.toString(group3));
+
+
+
+
+
 
 
 
