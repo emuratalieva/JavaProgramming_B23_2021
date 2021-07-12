103c0b1ed7730dc6711802826158823a895b8646
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-12 14:11:49 -0400

    day30

---
+++ src/day30_Recap/BranchingStatements.java
@@ -0,0 +1,24 @@
+package day30_Recap;
+
+public class BranchingStatements {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i<= 5; i++ ){
+            if(i == 3){
+             //   break; // exits the loop only
+             //   return; // exits the method
+             //   continue; // skips the current iteration
+            }
+            System.out.println(i);
+        }
+
+
+        System.out.println("Hello Everyone");
+
+
+
+    }
+
+}
