8872485abc4e3830296b68eddb0fc54c3e960d69
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-12 16:40:20 -0400

    day30

---
+++ src/utilities/ArraysUtility.java
@@ -0,0 +1,17 @@
+package utilities;
+
+public class ArraysUtility {
+
+
+    //1. create a function that can return the maximum number from an array of integer
+            // max( new int[]{10,20,30} ) ==> 30
+
+
+    //2. create a function that can return the minimum number from an array of integer
+        // min( new int[]{10,20,30} ) ==> 10
+
+
+
+
+
+}
