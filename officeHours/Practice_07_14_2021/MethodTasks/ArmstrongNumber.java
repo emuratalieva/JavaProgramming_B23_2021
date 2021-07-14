4dbb7738a52baf7daa7926fb4fec36449ab34c38
Mehmet <mhmtsytrk@gmail.com>
2021-07-14 17:38:07 -0400

    OfficeHours Day6 part 2

--- officeHours/Practice_07_14_2021/MethodTasks/ArmstrongNumber.java
+++ officeHours/Practice_07_14_2021/MethodTasks/ArmstrongNumber.java
@@ -10,23 +10,15 @@ public class ArmstrongNumber {
 
     }
     /*
-
 	Task 2 :   Write a method that can check if a number is Armstrong number
-
 				Note: if I have a 3-digit number then each of the digits is raised to the power of three and added to obtain a number.
 				If the number obtained equals the original number then, we call that armstrong number.
-
 				Armstrong numbers are     === 153 370 371 407 === for 3 digits
-
-
                 153   ==  (1*1*1)+(5*5*5)+(3*3*3)  == 153
 			                1       125      27
-
 			    3        153%10  = 3        153/10= 15
 			    5        15%10   = 5        15/10 = 1
 			    1        1%10    = 1         1/10 = 0
-
-
      */
 
     public static boolean isArmstrong(int number){
--- officeHours/Practice_07_14_2021/MethodTasks/PrimeNumber.java
+++ officeHours/Practice_07_14_2021/MethodTasks/PrimeNumber.java
@@ -11,10 +11,7 @@ public class PrimeNumber {
     }
     /*
     Task 1 :   Write a method that can check if a number is prime or not
-
 				Note : Prime numbers are special numbers, greater than 1, that have exactly two factors, themselves and 1
-
-
    10 -- 10 and 1 it is Prime     10 / 1-2-5-10
     7                             7  / 1-7              PrimeNumber
     13                            13/  1-13             PrimeNumber
