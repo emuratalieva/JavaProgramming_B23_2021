e40786524932f878da6e99cee4f0cf05fcbf8cef
Mehmet <mhmtsytrk@gmail.com>
2021-07-14 16:13:58 -0400

    OfficeHours Day6 part 2

---
+++ officeHours/Practice_07_14_2021/MethodsIntro/Deeper.java
@@ -0,0 +1,25 @@
+package Practice_07_14_2021.MethodsIntro;
+
+public class Deeper {
+
+    public static void main(String[] args) {
+        System.out.println("Not Deep");
+        deep();
+        System.out.println("Not Deep");
+
+    }
+
+    public static void deep() {
+        System.out.println(" Deep");
+        deeper();
+        System.out.println(" Deep");
+
+    }
+
+    public static void deeper() {
+        System.out.println(" Deeper");
+
+    }
+
+
+}
