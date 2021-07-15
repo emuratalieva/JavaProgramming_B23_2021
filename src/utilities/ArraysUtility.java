5d40ee3d03b5ab2b31ea70bac9c1bf37886b18a1
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-15 12:19:52 -0400

    Update ArraysUtility.java

--- src/utilities/ArraysUtility.java
+++ src/utilities/ArraysUtility.java
@@ -1,6 +1,53 @@
 package utilities;
 
+import java.util.Arrays;
+
 public class ArraysUtility {
+
+    //adds integer to an int array
+    public static int[] addElement(int[] array, int number){
+        /*
+        int[] result = new int[array.length+1];
+        int i = 0;
+        for (int each : array) {
+             result[i++] =each;
+        }
+
+        result[i] = number;
+         */
+
+        int[] result = Arrays.copyOf( array,   array.length+1 );
+        result[ result.length -1] = number;
+
+        return result;
+    }
+
+
+    //adds double to a double array
+    public static double[] addElement(double[] array,  double number){
+        double[] result =  Arrays.copyOf(array, array.length+1 );  //{1.5, 2.5, 3.5, 0.0};
+        result[result.length-1] = number; //{1.5, 2.5, 3.5, 4.5};
+
+        return result;
+    }
+
+
+    //adds String to a String array
+    public static String[] addElement(String[] array, String str){
+        String[] result = Arrays.copyOf(array, array.length + 1) ;//  { A, B, null }
+        result[result.length-1] = str; //  { A, B, C }
+        return result;
+    }
+
+    
+    // adds char to char array
+    public static char[] addElement(char[] array, char ch){
+        char[] result = Arrays.copyOf(array, array.length + 1) ;//  { A, B, null }
+        result[result.length-1] = ch; //  { A, B, C }
+        return result;
+    }
+
+
 
 
     //1. create a function that can return the maximum number from an array of integer
