df0db5fa2f88807d614c9a4400caa09bec14bcc6
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-15 10:33:11 -0400

    day32

---
+++ src/day32_MethodOverloading/WarmupTask1.java
@@ -0,0 +1,73 @@
+package day32_MethodOverloading;
+
+public class WarmupTask1 {
+
+    public static void main(String[] args) {
+        // 10+ 12;
+       int sum1 = sumOf2Numbers(10, 12);
+        System.out.println("sum1 = " + sum1);
+
+       // 100, 200, 300
+       int sum2 = sumOf3Numbers(100, 200, 300);
+        System.out.println("sum2 = " + sum2);
+
+       // 1000, 2000, 3000, 4000
+       int sum3 = sumOf4Numbers(1000, 2000, 3000, 4000);
+        System.out.println("sum3 = " + sum3);
+
+        System.out.println("---------------------------------------------");
+
+
+
+
+    }
+
+
+    public static int sumOf2Numbers(int a, int b){
+        return a+b;
+    }
+
+    public static int sumOf3Numbers(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public static int sumOf4Numbers(int a, int b, int c, int d){
+        return a+b+c+d;
+    }
+
+
+    //-----------------------------method overloading-----------------------------------
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+    public static int sum(int a, int b, int c ){
+        return a + b + c;
+    }
+
+    //                        1,    2      3       4
+    public static int sum(int a, int b, int c, int d){
+      //  return sum(a,b) + sum(c, d);
+     //   return  sum(  sum(a, b) ,   sum(c, d)  );
+        //   sum (       3,             7         )
+        //  10
+
+    //    return  sum(a,b,c) + d;
+        return a + b + c + d;
+    }
+    
+
+}
+
+/*
+Task1:
+    1. create a method that can find the sum of two numbers
+                    method name: sumOf2Numbers
+
+    2. create a method that can find the sum of three numbers
+                    method name: sumOf3Numbers
+
+    3. create a method that can find the sum of four numbers
+                    method name: sumOf4Numbers
+
+ */
