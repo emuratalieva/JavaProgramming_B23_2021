8840bc1b7f17827e306b8ac56b44b110d0dfd4eb
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-16 11:51:24 -0400

    day32

---
+++ src/day33_Methods/OverloadingMainMethod.java
@@ -0,0 +1,24 @@
+package day33_Methods;
+
+public class OverloadingMainMethod {
+
+
+    public static void main(char[] args) {
+
+        System.out.println("char");
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("String");
+
+    }
+
+    public static void main(int[] args) {
+        System.out.println("int");
+
+    }
+
+
+}
--- src/day33_Methods/WarmUpTask1.java
+++ src/day33_Methods/WarmUpTask1.java
@@ -1,10 +1,29 @@
 package day33_Methods;
 
+import java.util.Arrays;
+
 public class WarmUpTask1 {
 
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        numbers =  reverse(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+
+        char[] chars = {'A', 'B', 'C'};
+
+        chars = reverse(chars);
+
+
+
+    }
+
+
     //create a method that can reverse an array of integer
-    public static int[] reverse (int[] array){ //{1,2,3,4,5}
-        int[] result = new int[array.length]; //{0,0,0,0,0}
+    public static int[] reverse (int[] array){
+        int[] result = new int[array.length];
         for (int i = array.length-1, j=0; i >=0; i--, j++){
             result[j] = array[i];
         }
@@ -12,8 +31,8 @@ public class WarmUpTask1 {
     }
 
     //create a method that can reverse an array of double
-    public static double[] reverse (double[] array){ //{1,2,3,4,5}
-        double[] result = new double[array.length]; //{0,0,0,0,0}
+    public static double[] reverse (double[] array){
+        double[] result = new double[array.length];
         for (int i = array.length-1, j=0; i >=0; i--, j++){
             result[j] = array[i];
         }
@@ -21,8 +40,8 @@ public class WarmUpTask1 {
     }
 
     //create a method that can reverse an array of String
-    public static String[] reverse (String[] array){ //{1,2,3,4,5}
-        String[] result = new String[array.length]; //{0,0,0,0,0}
+    public static String[] reverse (String[] array){
+        String[] result = new String[array.length];
         for (int i = array.length-1, j=0; i >=0; i--, j++){
             result[j] = array[i];
         }
@@ -30,8 +49,8 @@ public class WarmUpTask1 {
     }
 
     //create a method that can reverse an array of char
-    public static char[] reverse (char[] array){ //{1,2,3,4,5}
-        char[] result = new char[array.length]; //{0,0,0,0,0}
+    public static char[] reverse (char[] array){
+        char[] result = new char[array.length];
         for (int i = array.length-1, j=0; i >=0; i--, j++){
             result[j] = array[i];
         }
