8ea9afb579c76398df4233786666cfb521249866
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-16 11:53:40 -0400

    day32

--- src/utilities/ArraysUtility.java
+++ src/utilities/ArraysUtility.java
@@ -251,6 +251,42 @@ public class ArraysUtility {
     }
 
 
+    //reverses the given integer array
+    public static int[] reverse (int[] array){
+        int[] result = new int[array.length];
+        for (int i = array.length-1, j=0; i >=0; i--, j++){
+            result[j] = array[i];
+        }
+        return result;
+    }
+
+    //reverses the given double array
+    public static double[] reverse (double[] array){
+        double[] result = new double[array.length];
+        for (int i = array.length-1, j=0; i >=0; i--, j++){
+            result[j] = array[i];
+        }
+        return result;
+    }
+
+    //reverses the given String array
+    public static String[] reverse (String[] array){
+        String[] result = new String[array.length];
+        for (int i = array.length-1, j=0; i >=0; i--, j++){
+            result[j] = array[i];
+        }
+        return result;
+    }
+
+    //reverses the given char array
+    public static char[] reverse (char[] array){
+        char[] result = new char[array.length];
+        for (int i = array.length-1, j=0; i >=0; i--, j++){
+            result[j] = array[i];
+        }
+        return result;
+    }
+
 
 
 }
