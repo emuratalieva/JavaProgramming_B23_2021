52fe5fcc72f5c03f5447f752180a6e5e939cd841
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-16 10:54:54 -0400

    day32

--- src/utilities/ArraysUtility.java
+++ src/utilities/ArraysUtility.java
@@ -161,9 +161,96 @@ public class ArraysUtility {
 
         return arr3;
     }
-    
-    
-    
-    
+
+
+    // remove the element at given index of the array
+    public static int[] remove(int[] array, int index){
+
+        if(index < 0 || index > array.length -1 ){ // if the given index number is invalid
+            System.err.println("Invalid Index");
+            System.exit(0); // exits the system
+        }
+
+        int[] result = new int[array.length-1];
+
+        for(int i = 0, j = 0; i < array.length; i++ ){
+            if(i == index){ // moves all the elements of array except the element at given index
+                continue;
+            }else{
+                result[j++]  = array[i];
+            }
+        }
+
+        return result;
+    }
+
+
+    // remove the element at given index of the array
+    public static double[] remove(double[] array, int index){
+
+        if(index < 0 || index > array.length -1 ){ // if the given index number is invalid
+            System.err.println("Invalid Index");
+            System.exit(0); // exits the system
+        }
+
+        double[] result = new double[array.length-1];
+
+        for(int i = 0, j = 0; i < array.length; i++ ){
+            if(i == index){ // stores all the elements of array except the element at given index
+                continue;
+            }else{
+                result[j++]  = array[i];
+            }
+        }
+
+        return result;
+    }
+
+
+    // remove the element at given index of the array
+    public static String[] remove(String[] array, int index){
+
+        if(index < 0 || index > array.length -1 ){ // if the given index number is invalid
+            System.err.println("Invalid Index");
+            System.exit(0); // exits the system
+        }
+
+        String[] result = new String[array.length-1];
+
+        for(int i = 0, j = 0; i < array.length; i++ ){
+            if(i == index){ // stores all the elements of array except the element at given index
+                continue;
+            }else{
+                result[j++]  = array[i];
+            }
+        }
+
+        return result;
+    }
+
+
+    // remove the element at given index of the array
+    public static char[] remove(char[] array, int index){
+
+        if(index < 0 || index > array.length -1 ){ // if the given index number is invalid
+            System.err.println("Invalid Index");
+            System.exit(0); // exits the system
+        }
+
+        char[] result = new char[array.length-1];
+
+        for(int i = 0, j = 0; i < array.length; i++ ){
+            if(i == index){ // stores all the elements of array except the element at given index
+                continue;
+            }else{
+                result[j++]  = array[i];
+            }
+        }
+
+        return result;
+    }
+
+
+
 
 }
