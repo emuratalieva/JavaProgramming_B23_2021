e2a25558c5e149ca23aa8a9c7c36d7a706532f33
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-22 10:29:06 -0400

    day34

---
+++ src/day34_LocalDateTime_Wrapper/LocalDateIntro.java
@@ -0,0 +1,24 @@
+package day34_LocalDateTime_Wrapper;
+
+import java.time.LocalDate;
+
+public class LocalDateIntro {
+
+    public static void main(String[] args) {
+
+      LocalDate eid = LocalDate.of(2021, 7, 20);
+
+        System.out.println(eid);
+
+      LocalDate today = LocalDate.now();
+
+        System.out.println(today);
+
+
+
+
+
+    }
+
+
+}
